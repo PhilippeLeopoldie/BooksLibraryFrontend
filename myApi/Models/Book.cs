5a4d98b030daf1caf630e9bac1de6a34cd03d34b
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-05-23 15:36:26 +0200

    refactor

--- myApi/Models/Book.cs
+++ myApi/Models/Book.cs
@@ -9,12 +9,6 @@ public class Book
     public string? Title { get; set; }
     public string? Author { get; set ; }
     
-    //public bool Borrowed {get; set; } = false;
-   // public DateTime? BorrowedTime {get; set; }
-    //public bool GivenBack {get; set; } = false;
-    //public DateTime? GivenBackTime {get; set;}
-
-   
     public List<Opinion>? Opinions { get; set; }
 
     //public int? UserId { get; set; }
