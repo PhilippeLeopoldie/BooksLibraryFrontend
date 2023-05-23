80758d72ddfe3a9cb000cc86fd24a578802ea747
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-05-23 20:06:42 +0200

    generate context

--- backend/LibraryBackend/Program.cs
+++ backend/LibraryBackend/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyLibraryContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("myLibraryContext")));
 
 // Add services to the container.
 
