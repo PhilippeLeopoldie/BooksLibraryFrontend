af65214e990b55b400f7e24ca8ee4bbac21489f1
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-05-25 06:15:34 +0200

    applying the cors policy

--- backend/LibraryBackend/Program.cs
+++ backend/LibraryBackend/Program.cs
@@ -32,6 +32,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -42,6 +44,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
