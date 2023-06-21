21da6f422cca61efb872386556b69f60a110c8fc
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-06-21 17:52:45 +0200

    update books component with new endpoint

--- frontend/LibrairyFrontEnd/src/Component/Books.tsx
+++ frontend/LibrairyFrontEnd/src/Component/Books.tsx
@@ -6,24 +6,34 @@ import { FetchOpinions } from "./FetchOpinion";
 import trash from "../media/delete.svg";
 
 function Books() {
-  
-  const [Opinions, setOpinions] = useState<OpinionType[]>();
-  const [books, setBooks] = useState<BookType[]>();
+  const [opinions, setOpinions] = useState<OpinionType[]>();
+  const [books, setBooks] = useState<BookType[]>([]);
   const random = books?.at(Math.floor(Math.random() * books.length));
+ 
 
   useEffect(() => {
-    FetchApi("https://booklibray-backend.herokuapp.com/api/Book").then((books) =>
-      setBooks(books)
-    );
-    FetchApi("https://booklibray-backend.herokuapp.com/api/Opinion").then(
-      (opinions) => setOpinions(opinions)
-    );
+    const fetchBooks = async () => {
+      try {
+        const booksData = await FetchApi("https://leopoldie-booklibrary-backend.herokuapp.com/api/Book");
+        setBooks(booksData.$values);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+      }
+    };
+  
+    fetchBooks();
+    FetchApi(
+      "https://leopoldie-booklibrary-backend.herokuapp.com/api/Opinion"
+    ).then((opinions) => setOpinions(opinions));
   }, []);
 
   const DeleteBook = async (bookId: number) => {
-    await fetch(`https://booklibray-backend.herokuapp.com/api/Book/${bookId}`, {
-      method: "DELETE",
-    }).then(() => {
+    await fetch(
+      `https://leopoldie-booklibrary-backend.herokuapp.com/api/Book/${bookId}`,
+      {
+        method: "DELETE",
+      }
+    ).then(() => {
       window.location.reload();
     });
   };
@@ -33,29 +43,30 @@ function Books() {
       <h2>Recommendation of the day</h2>
       <h3>{random?.title}</h3>
       <div className="bookcontainer">
-        {books?.map((book, index) => (
-          <div className="bookcard">
-            <div className="bookcard--header">
-              <button
-                className=" button booktitle--trashbutton"
-                type="submit"
-                onClick={async () => {
-                  await DeleteBook(book.bookId);
-                }}
-              >
-                <img className="icone bookcard--iconeTrash" src={trash} />
-              </button>
-              <h2 className="booktitle" key={index}>
-                {book.title}
-              </h2>
-              <h3 className="bookauthor" key={index}>
-                by: {book.author}
-              </h3>
-            </div>
+        {books &&
+          books.map((book, index) => (
+            <div className="bookcard" key={book.bookId}>
+              <div className="bookcard--header">
+                <button
+                  className=" button booktitle--trashbutton"
+                  type="submit"
+                  onClick={async () => {
+                    await DeleteBook(book.bookId);
+                  }}
+                >
+                  <img className="icone bookcard--iconeTrash" src={trash} />
+                </button>
+                <h2 className="booktitle" >
+                  {book.title}
+                </h2>
+                <h3 className="bookauthor">
+                  by: {book.author}
+                </h3>
+              </div>
 
-            <FetchOpinions bookId={book.bookId} />
-          </div>
-        ))}
+              <FetchOpinions bookId={book.bookId} />
+            </div>
+          ))}
       </div>
     </div>
   );
