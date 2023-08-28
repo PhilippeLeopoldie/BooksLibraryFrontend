0401a0d3f0c97bdbbd60457145c2e1dc715f00c2
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-08-28 18:31:58 +0200

    delete depracated folders

--- deprecated_myApi/DTO/BookAddRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-namespace myApi.DTO;
-
-public class BookAddRequest
-{   
-    
-    public string? Title { get; set; }
-    public string? Author { get; set ; }
-}
