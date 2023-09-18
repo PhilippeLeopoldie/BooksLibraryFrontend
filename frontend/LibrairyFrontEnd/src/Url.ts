68f0ec9f0853be4f48b85f6b4fd43ae552855549
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-18 15:33:03 +0200

    update backend url

--- frontend/LibrairyFrontEnd/src/Url.ts
+++ frontend/LibrairyFrontEnd/src/Url.ts
@@ -1,4 +1,4 @@
-const url = "https://leopoldie-booklibrary-backend.herokuapp.com/"
+const url = "https://booklibrary-backend-20f7a19cecb2.herokuapp.com/"
 
 //const url = "https://localhost:7152/"
 
