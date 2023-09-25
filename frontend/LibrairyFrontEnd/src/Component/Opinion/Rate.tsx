6d373463b25c4a7cce39adcc500bb867310aa4c8
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-25 23:27:30 +0200

    Implement rate component with Unicode star symbol

---
+++ frontend/LibrairyFrontEnd/src/Component/Opinion/Rate.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Rate.css"
+
+type RateType = {
+  rate: number;
+};
+
+export function Rate({ rate }: RateType) {
+  const rateArray = new Array<number>(rate).fill(1);
+
+  return (
+    <>
+    <div className="rate_container--flex">
+    {rateArray.map((rate, index) => (
+        <div className='rate_star' key={index}> &#9733;</div>
+      ))}
+    </div>
+      
+    </>
+  );
+}
