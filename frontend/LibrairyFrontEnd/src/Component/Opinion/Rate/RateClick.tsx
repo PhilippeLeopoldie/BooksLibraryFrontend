030e449c34c3c3619ac8d2a73c347d5464c47e32
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-27 00:30:23 +0200

    Implemente RateClick function

---
+++ frontend/LibrairyFrontEnd/src/Component/Opinion/Rate/RateClick.tsx
@@ -0,0 +1,39 @@
+import { useState, useContext } from "react";
+import "./Rate.css";
+
+type RateType = {
+  rate: number;
+  OpinionContextRate: Function;
+};
+
+export function RateClick({ rate, OpinionContextRate }: RateType) {
+  const numberOfStars: number = 5;
+  const emptyStars = new Array<number>(numberOfStars).fill(0);
+  const [filledStars, setFilledStars] = useState<number[]>(
+    emptyStars.fill(1, 0, rate)
+  );
+
+  const toggleStar = (index: number) => {
+    const updatedStars = [...filledStars];
+    updatedStars[index] = updatedStars[index] === 1 ? 0 : 1;
+    setFilledStars(updatedStars);
+    const newRate: number = updatedStars.reduce((a, b) => a + b, 0);
+    OpinionContextRate(newRate);
+  };
+
+  return (
+    <>
+      <div className="rate_container--flex">
+        {filledStars.map((isFilled, index) => (
+          <div
+            className={`rate_star ${isFilled ? "filled" : "empty"}`}
+            key={index}
+            onClick={() => toggleStar(index)}
+          >
+            &#9733;
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
