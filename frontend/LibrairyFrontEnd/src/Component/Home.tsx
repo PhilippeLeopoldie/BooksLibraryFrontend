012a6e895bf6e262e7d10587eef99c4719578481
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-28 18:16:33 +0200

    Remove unused code

--- frontend/LibrairyFrontEnd/src/Component/Home.tsx
+++ frontend/LibrairyFrontEnd/src/Component/Home.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route,Link, useNavigate } from 'react-router-dom'
 import {Books} from './Book/Books'
-import {AddBook} from './Book/AddBook'
+
 
 export const Home = () => {
   
