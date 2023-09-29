3ccf455b66f1d0c4074377fad16feaee0458f41b
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-30 01:33:04 +0200

    Add onEdit function as a prop to OpinionEdit

--- frontend/LibrairyFrontEnd/src/Component/Opinion/OpinionEdit.tsx
+++ frontend/LibrairyFrontEnd/src/Component/Opinion/OpinionEdit.tsx
@@ -5,13 +5,17 @@ import url from "../../Url";
 import { RateClick } from "./Rate/RateClick";
 import { useNavigate } from "react-router";
 
-export const OpinionEdit = () => {
+type EditOpinionHandler = {
+  onEdit: Function;
+};
+
+export const OpinionEdit = ({ onEdit }: EditOpinionHandler) => {
   const opinionContext = useContext(OpinionContext);
-  const navigate = useNavigate()
-  const [updatedRate, setUpdatedRate] = useState<number>(opinionContext.rate)
+  const navigate = useNavigate();
+  const [updatedRate, setUpdatedRate] = useState<number>(opinionContext.rate);
   const handleOpinionContextRate = (newRate: number) => {
-    setUpdatedRate(newRate)
-  }
+    setUpdatedRate(newRate);
+  };
 
   const [view, setView] = useState<string>(opinionContext.view);
   const [userName, setUserName] = useState<string>(opinionContext.userName);
@@ -32,17 +36,19 @@ export const OpinionEdit = () => {
           bookId: opinionContext.bookId,
         }),
       };
-      const response : Response = await fetch(
-        url+`api/Opinion/${opinionContext.id}`,requestOptions);
-        if(response.status === 200) {
-           navigate("/");
-          } else if(response.status === 400){
-            const errorData = await response.json();
-            setErrorDetail(errorData.detail);
-          }
-    } catch (error: any ) {
-        console.error("An error occurred:", error.message);
-        setErrorDetail(error.message);
+      const response: Response = await fetch(
+        url + `api/Opinion/${opinionContext.id}`,
+        requestOptions
+      );
+      if (response.status === 200) {
+        onEdit();
+      } else if (response.status === 400) {
+        const errorData = await response.json();
+        setErrorDetail(errorData.detail);
+      }
+    } catch (error: any) {
+      console.error("An error occurred:", error.message);
+      setErrorDetail(error.message);
     }
   };
   return (
@@ -68,24 +74,22 @@ export const OpinionEdit = () => {
             setErrorDetail("")
           }}
         />
-        <RateClick rate={opinionContext.rate} OpinionContextRate={handleOpinionContextRate}/>
+        <RateClick
+          rate={opinionContext.rate}
+          OpinionContextRate={handleOpinionContextRate}
+        />
 
         <button
           className="button validationButton"
-          onClick={(e)=> {
-
-            updateOpinion(e);
-            
-          }}
+          onClick={(e) => updateOpinion(e)}
           type="submit"
         >
           <h2 className="validationButton__name">Save Changes</h2>
-          
         </button>
-        { errorDetail && <div className="validation__errorMessage">{errorDetail}</div>
-          
-        }
+        {errorDetail && (
+          <div className="validation__errorMessage">{errorDetail}</div>
+        )}
       </form>
     </>
   );
-}
+};
