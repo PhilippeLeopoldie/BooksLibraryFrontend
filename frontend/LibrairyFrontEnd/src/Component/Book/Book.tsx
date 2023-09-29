fd28f64eacda22d8587c147908955ec4a7e12cea
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-30 01:18:10 +0200

    Add toggleEditOpinion() at Book and indent code

--- frontend/LibrairyFrontEnd/src/Component/Book/Book.tsx
+++ frontend/LibrairyFrontEnd/src/Component/Book/Book.tsx
@@ -1,24 +1,31 @@
 import { FetchOpinions } from "../Opinion/FetchOpinion";
 import url from "../../Url";
 import trash from "../../media/delete.svg";
+import { useState } from "react";
+import { OpinionEdit } from "../Opinion/OpinionEdit";
 
 type BookWithDeletionHandler = {
-  book : {
-    id: number,
-    title: string,
-    author: string
-  }
-  onDelete : Function
-}
+  book: {
+    id: number;
+    title: string;
+    author: string;
+  };
+  onDelete: Function;
+};
 
-export const Book = ({ book, onDelete} : BookWithDeletionHandler) => {
+export const Book = ({ book, onDelete }: BookWithDeletionHandler) => {
+  const [editHandling, setEditHandling] = useState<Boolean>(false);
+
+  const toggleEditOpinion = () => {
+    setEditHandling(!editHandling);
+  };
+
   const DeleteBook = async (bookId: number) => {
     try {
       await fetch(url + `api/Book/${bookId}`, {
         method: "DELETE",
       });
-      onDelete(bookId)
-       
+      onDelete(bookId);
     } catch (error) {
       console.error("Error deleting book:", error);
     }
@@ -26,28 +33,30 @@ export const Book = ({ book, onDelete} : BookWithDeletionHandler) => {
 
   return (
     <>
-      <div className="bookcard" key={book.id}>
-        <div className="bookcard--header">
-          <button
-            className=" button booktitle--trashbutton"
-            type="submit"
-            onClick={async () => {
-              await DeleteBook(book.id);
-            }}
-          >
-            <img
-              className="icone bookcard--iconeTrash"
-              src={trash}
-              alt="delete"
-            />
-          </button>
-          <h2 className="booktitle">{book.title}</h2>
-          <h3 className="bookauthor">by: {book.author}</h3>
+      {!editHandling ? (
+        <div className="bookcard" key={book.id}>
+          <div className="bookcard--header">
+            <button
+              className=" button booktitle--trashbutton"
+              type="submit"
+              onClick={async () => {
+                await DeleteBook(book.id);
+              }}
+            >
+              <img
+                className="icone bookcard--iconeTrash"
+                src={trash}
+                alt="delete"
+              />
+            </button>
+            <h2 className="booktitle">{book.title}</h2>
+            <h3 className="bookauthor">by: {book.author}</h3>
+          </div>
+          <FetchOpinions bookId={book.id} onEdit={toggleEditOpinion} />
         </div>
-        <FetchOpinions bookId={book.id} />
-      </div>
+      ) : (
+        <OpinionEdit onEdit={toggleEditOpinion} />
+      )}
     </>
   );
 };
-
-
