6e61e48ccecd4eaea9b7f40a0a41eaece885b10d
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-29 16:44:34 +0200

    Rename Books into BookList

---
+++ frontend/LibrairyFrontEnd/src/Component/BookList/BookList.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { BookType} from "../../Type";
+import FetchApi from "../../FetchApi";
+import url from "../../Url";
+import {Book} from "../Book/Book";
+
+
+export const Books = () => {
+  const [books, setBooks] = useState<BookType[]>([]);
+  const random = books?.at(Math.floor(Math.random() * books.length));
+
+  const handleDeleteBook = (bookId : number) => {
+    setBooks((books) => books.filter((book) => book.book.id !== bookId))
+  }
+  const fetchBooks = async () => {
+    try {
+      const booksData = await FetchApi(url + "api/Book");
+      setBooks(booksData.$values);  
+    } catch (error) {
+      console.error("Error fetching books:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  return (
+    <div className="books">
+      <h2>Recommendation of the day</h2>
+      <h3>{random?.book.title}</h3>
+      <div className="bookcontainer">
+        {books &&
+          books
+          .sort((a,b) => b.book.id - a.book.id)
+          .map((bookDetail, index) => (
+            <Book book = {bookDetail.book} onDelete ={handleDeleteBook}/>
+          ))}
+      </div>
+    </div>
+  );
+}
