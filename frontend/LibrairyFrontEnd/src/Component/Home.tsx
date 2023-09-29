62cd9916a2bb02ec0b706f7d646ff10b24ef33d4
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-30 01:10:38 +0200

    Delete unused code and empty lines

--- frontend/LibrairyFrontEnd/src/Component/Home.tsx
+++ frontend/LibrairyFrontEnd/src/Component/Home.tsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
 import {BookList} from './BookList/BookList'
 
-
-
-
 export const Home = () => {
-
   
   return (
-    <div>
+    <>
       <BookList/>
-    </div>
+    </>
   )
 }
-
-
