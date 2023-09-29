8f18693e0d903f9173ea492e04b325585409e275
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-09-29 16:58:23 +0200

    Rename Books into BookList

--- frontend/LibrairyFrontEnd/src/Component/Home.tsx
+++ frontend/LibrairyFrontEnd/src/Component/Home.tsx
@@ -1,11 +1,18 @@
-import {Books} from './Book/Books'
+import { useState } from 'react'
+import {Books} from './BookList/BookList'
+import { Parent } from './Book/Parent'
+import { EditChild } from './Book/EditChild'
+
 
 
 export const Home = () => {
+
   
   return (
     <div>
+      
       <Books/>
+      
     </div>
   )
 }
