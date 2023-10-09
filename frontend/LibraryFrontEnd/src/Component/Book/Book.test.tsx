1867b1be65c1f1ac604054e8b3845555c7f48b8d
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-09 17:07:21 +0200

    Add test to check trash image rendering

--- frontend/LibraryFrontEnd/src/Component/Book/Book.test.tsx
+++ frontend/LibraryFrontEnd/src/Component/Book/Book.test.tsx
@@ -1,28 +1,48 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, getByAltText } from '@testing-library/react';
 import {Book} from './Book';
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
+import trash from "../../media/delete.svg";
+//import { Trash } from '../mocks/Trash';
 
+
+
+// Arrange : Define the actual book data for testing
+const actualBookData = {
+  id: 1,
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+};
 
 test('renders book title and author', () => {
-  // Define the actual book data for testing
-  const actualBookData = {
-    id: 1,
-    title: 'The Great Gatsby',
-    author: 'F. Scott Fitzgerald',
-  };
-
-  // Render the Book component with the actual book data
+  
+    // Render the Book component with the actual book data
   render(<Book book={actualBookData} onDelete={() => {}} />);
 
-  // Use screen.getByText to find elements by their text content
+  // Act: Use screen.getByText to find elements by their text content
   const titleElement = screen.getByText('The Great Gatsby');
   const authorElement = screen.getByText('by: F. Scott Fitzgerald');
 
-  // Assert that the elements are in the document
+  // Assert
   expect(titleElement).toBeInTheDocument();
   expect(authorElement).toBeInTheDocument();
-
-  // Assert that the elements have the expected class names
   expect(titleElement).toHaveClass('booktitle');
   expect(authorElement).toHaveClass('bookauthor');
 });
+
+test('renders an Trash with the correct alt text and ClassName', ()=> {
+  // Arrange
+  const onDelete = jest.fn()
+
+  const {getByAltText, container} = render(<Book book={actualBookData} onDelete={onDelete} />);
+  
+
+  // Act
+  const trashImage = getByAltText('delete');
+  
+  //Assert
+  expect(trashImage).toBeInTheDocument();
+  expect(trashImage).toHaveClass('bookcard--iconeTrash');
+  
+})
+
+
