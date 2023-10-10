5406cc09a851f8eb1b691203369d16b01616082d
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-10 16:06:06 +0200

    Implement Opinion test for  'Loading...' message while fetching data

---
+++ frontend/LibraryFrontEnd/src/Component/Opinion/Opinion.test.tsx
@@ -0,0 +1,27 @@
+import { Opinion } from "./Opinion";
+import { act, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+global.fetch = jest.fn().mockImplementation(() =>
+  Promise.resolve({
+    json: () =>
+      Promise.resolve([
+        { Opinion: { id: 1, view: "View 1", userName: "userName 1", rate: 2 } },
+      ]),
+    status: 200,
+    hearders: new Headers(),
+  })
+);
+
+test("Should render Loading message initially", async () => {
+  //Arrange
+  await act(async () => {
+    render(<Opinion bookId={1} onEdit={() => {}} />);
+  });
+
+  //Act
+  const loadingMessage = screen.getByText("Loading...");
+
+  //Expect
+  expect(loadingMessage).toBeInTheDocument();
+});
