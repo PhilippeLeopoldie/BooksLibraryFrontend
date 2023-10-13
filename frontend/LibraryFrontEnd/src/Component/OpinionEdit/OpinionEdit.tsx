cab3454281ef9e1ba280a96dfef41b5e110e88ae
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-13 20:06:06 +0200

    replace context by toEdit fonction as callback from Book

--- frontend/LibraryFrontEnd/src/Component/OpinionEdit/OpinionEdit.tsx
+++ frontend/LibraryFrontEnd/src/Component/OpinionEdit/OpinionEdit.tsx
@@ -1,21 +1,36 @@
 import { SyntheticEvent, useState, useContext } from "react";
-import { OpinionContext } from "../../Context";
 import url from "../../Url";
 import { RateClick } from "./RateClick/RateClick";
 
-type EditOpinionHandler = {
-  onEdit:()=> void;
+type EditedOpinion = {
+  id:number
+  rate: number,
+  view: string,
+  userName: string,
+  bookId: number
 };
 
-export const OpinionEdit = ({ onEdit }: EditOpinionHandler) => {
-  const opinionContext = useContext(OpinionContext);
-  const [updatedRate, setUpdatedRate] = useState<number>(opinionContext.rate);
+
+type EditOpinionHandler = {
+  toEdit:(opinionToUpdate:EditedOpinion) => void,
+  opinion:{
+    id:number,
+    view:string,
+    userName:string,
+    rate:number,
+    bookId:number,
+  }
+  
+};
+
+export const OpinionEdit = ({ toEdit,opinion}: EditOpinionHandler) => {
+  const [updatedRate, setUpdatedRate] = useState<number>(opinion.rate);
   const handleOpinionContextRate = (newRate: number) => {
     setUpdatedRate(newRate);
   };
 
-  const [view, setView] = useState<string>(opinionContext.view);
-  const [userName, setUserName] = useState<string>(opinionContext.userName);
+  const [view, setView] = useState<string>(opinion.view);
+  const [userName, setUserName] = useState<string>(opinion.userName);
   const [errorDetail, setErrorDetail] = useState<string>("");
 
   const updateOpinion = async (e: SyntheticEvent) => {
@@ -30,15 +45,22 @@ export const OpinionEdit = ({ onEdit }: EditOpinionHandler) => {
           view: view,
           userName: userName,
           rate: updatedRate,
-          bookId: opinionContext.bookId,
+          bookId: opinion.bookId,
         }),
       };
       const response: Response = await fetch(
-        url + `api/Opinion/${opinionContext.id}`,
+        url + `api/Opinion/${opinion.id}`,
         requestOptions
       );
       if (response.status === 200) {
-        onEdit();
+        const opinionToEdit:EditedOpinion = {
+          id:opinion.id,
+          rate: updatedRate,
+          view: view,
+          userName:userName,
+          bookId:opinion.bookId
+        }
+        toEdit(opinionToEdit);
       } else if (response.status === 400) {
         const errorData = await response.json();
         setErrorDetail(errorData.detail);
@@ -72,7 +94,7 @@ export const OpinionEdit = ({ onEdit }: EditOpinionHandler) => {
           }}
         />
         <RateClick
-          rate={opinionContext.rate}
+          rate={updatedRate}
           HandleRate={handleOpinionContextRate}
         />
 
