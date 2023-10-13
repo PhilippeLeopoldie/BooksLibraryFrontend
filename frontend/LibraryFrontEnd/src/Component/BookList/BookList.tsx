c0290aba15060050e4deb5018162de20ea05a6da
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-13 20:12:30 +0200

    refactor: indentation

--- frontend/LibraryFrontEnd/src/Component/BookList/BookList.tsx
+++ frontend/LibraryFrontEnd/src/Component/BookList/BookList.tsx
@@ -3,29 +3,24 @@ import { BookType } from "../../Type";
 import url from "../../Url";
 import { useEffect, useState } from "react";
 
-
 export const BookList = () => {
   const [books, setBooks] = useState<BookType[] | null>(null);
 
   const handleDeleteBook = (bookId: number) => {
-
     setBooks((books) => {
-      if(books!==null) {
+      if (books !== null) {
         return books.filter((book) => book.book.id !== bookId);
       } else {
-        return[]
-    }
-  }) 
-
+        return [];
+      }
+    });
   };
-    
-  
 
   const fetchBooks = async () => {
     try {
-      const booksResponse : Response = (await fetch(url + "api/Book"));
-      if(booksResponse.status === 200) {
-        const booksResponseData  = await booksResponse.json();
+      const booksResponse: Response = await fetch(url + "api/Book");
+      if (booksResponse.status === 200) {
+        const booksResponseData = await booksResponse.json();
         setBooks(booksResponseData.$values);
       } else if (booksResponse.status === 404) {
         console.log(booksResponse);
@@ -35,27 +30,30 @@ export const BookList = () => {
     }
   };
 
-  
   useEffect(() => {
     fetchBooks();
   }, []);
 
-  if(!books) {
-    return <h1>Loading...</h1>
+  if (!books) {
+    return <h1>Loading...</h1>;
   }
-  
 
-  const random = books?.at(Math.floor(Math.random() * books.length));
+  const randomIndex = (Math.floor(Math.random() * books.length));
+  const random = books[randomIndex]
   return (
     <div className="books">
       <h2>Recommendation of the day</h2>
       <h3>{random?.book.title}</h3>
-      <div className="booksContainer">
+      <div className="booksContainer" data-testid="booksContainer">
         {books &&
           books
             .sort((a, b) => b.book.id - a.book.id)
             .map((bookDetail, index) => (
-              <Book key={bookDetail.book.id} book={bookDetail.book} onDelete={handleDeleteBook} />
+              <Book
+                key={bookDetail.book.id}
+                book={bookDetail.book}
+                onDelete={handleDeleteBook}
+              />
             ))}
       </div>
     </div>
