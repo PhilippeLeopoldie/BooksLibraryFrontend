dededfb5fc870af29f1c75bb13e77a80a80bba62
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-16 16:29:12 +0200

    Update Opinion: display lastOpinion by default when available

--- frontend/LibraryFrontEnd/src/Component/Opinion/Opinion.tsx
+++ frontend/LibraryFrontEnd/src/Component/Opinion/Opinion.tsx
@@ -12,6 +12,15 @@ type EditOpinion = {
   userName: string,
   bookId: number
 };
+
+type Opinion = {
+  id: number,
+  rate: number,
+  view: string,
+  userName: string,
+  bookId: number
+};
+
 type BookType = {
   bookId: number;
   onEdit: (opinionToUpdate:EditOpinion) => void
@@ -19,8 +28,8 @@ type BookType = {
 
 
 export const Opinion = ({bookId, onEdit}: BookType) => {
-  const [Opinions, setOpinions] = useState<OpinionType[] | null >(null);
-
+  const [opinions, setOpinions] = useState<OpinionType[] | null >(null);
+  const [lastOpinion, setLastOpinion] = useState<Opinion | null>(null);
   const FetchOpinions = async (bookId :number) => {
    try {
     const response : Response = await fetch(url + "api/Opinion/BookId=" + bookId);
@@ -40,34 +49,38 @@ export const Opinion = ({bookId, onEdit}: BookType) => {
     FetchOpinions(bookId);
   }, []);
 
-  if(!Opinions) {
+  useEffect(()=> {
+    opinions && opinions.length > 0 && setLastOpinion(opinions[opinions.length-1])
+  },[opinions])
+  
+  if(!opinions) {
     return <h2 className="OpinionLoading">Loading...</h2>
   }
 
   return (
     <>
       <div className="opinioncontainer">
-        {Opinions && Opinions.map((opinion, index) => (
-          <div className="opinioncontainer--card" key={opinion.id}>
+        {lastOpinion && (
+          <div className="opinioncontainer--card" key={lastOpinion.id}>
             <div className="opinionCardItems" >
               <textarea
                 className="opinionCardItems opinioncard--view"
-                value={opinion.view}
+                value={lastOpinion.view}
                 readOnly
               />
-              <div className="opinionCardItems">{opinion.userName}</div>
-              <Rate rate= {opinion.rate}/>
+              <div className="opinionCardItems">{lastOpinion.userName}</div>
+              <Rate rate= {lastOpinion.rate}/>
             </div>
             <div className="opinionCardItems opinioncard--footer">
               <button
                 className="button opinioncard--buttonmodify"
                 onClick={() => {
                   const opinionToEdit:EditOpinion = {
-                    id:opinion.id,
-                    rate: opinion.rate,
-                    view: opinion.view,
-                    userName:opinion.userName,
-                    bookId:opinion.bookId
+                    id:lastOpinion.id,
+                    rate: lastOpinion.rate,
+                    view: lastOpinion.view,
+                    userName:lastOpinion.userName,
+                    bookId:lastOpinion.bookId
                   }
                   onEdit(opinionToEdit);
                 }}
@@ -77,7 +90,7 @@ export const Opinion = ({bookId, onEdit}: BookType) => {
               </button>
             </div>
           </div>
-        ))}
+        )}
       </div>
     </>
   );
