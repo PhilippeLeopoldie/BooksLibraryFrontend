09252cb2e5d7012151a36955ac42f6091e63c3ee
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-18 01:03:18 +0200

    Add SVG icons & search route to navbar, rename classes, & add CSS styles

--- frontend/LibraryFrontEnd/src/Component/App/App.tsx
+++ frontend/LibraryFrontEnd/src/Component/App/App.tsx
@@ -2,32 +2,66 @@ import "./App.css";
 import { AddBook } from "../BookCreate/BookCreate";
 import { BookList } from "../BookList/BookList";
 import { Routes, BrowserRouter, Route, Link } from "react-router-dom";
-import home from "../../media/home_42px.svg";
+import { BookSearch } from "../BookSearch/BookSearch";
+import addIcone from "../../media/add64px.svg";
+import bookIcone from "../../media/book64px.svg";
+import homeIcone from "../../media/home_42px.svg";
+import searchIcone from "../../media/search64px.svg";
 
 export const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="nav">
-          <ul className="liste">
-            <dl className="nav-data">
-              <Link className="nav-link home" to="/">
-                <img
-                className="nav__HomeIcone"
-                src={home}
-                ></img>
-              </Link>
-            </dl>
-            <dl className="nav-data">
-              <Link className="nav-link" to="/addBook">
-                Add a Book
-              </Link>
-            </dl>
-          </ul>
+        <div className="navbar">
+          <dl className="navElement navbar__home">
+            <Link className="nav-link homeLink" to="/">
+              <img className="nav__HomeIcone" src={homeIcone} alt="Home"></img>
+              <figcaption className="iconeTitle nav__Home__title">
+                Home
+              </figcaption>
+            </Link>
+          </dl>
+          <dl className="navElement navbar__search">
+            <Link className="nav-link" to="/Search">
+              <img
+                className="nav-link nav__search"
+                width="48"
+                height="48"
+                src={searchIcone}
+                alt="Search"
+              />
+              <figcaption className="iconeTitle nav__search__title">
+                Search
+              </figcaption>
+            </Link>
+          </dl>
+          <dl className="navElement navbar__addBook">
+            <Link className="nav-link nav__addBook" to="/addBook">
+              <img
+                className="nav-link nav__book"
+                width="48"
+                height="48"
+                src={bookIcone}
+                alt="book"
+              />
+
+              <img
+                className="nav-link"
+                width="20"
+                height="20"
+                src={addIcone}
+                alt="add"
+              />
+              <figcaption className="iconeTitle nav__Add__title">
+                Add a book
+              </figcaption>
+            </Link>
+          </dl>
         </div>
         <Routes>
-          <Route path="/" element={<BookList />}></Route>
-          <Route path="/addBook" element={<AddBook />}></Route>
+          <Route path="/" element={<BookList />}/>
+          <Route path="/addBook" element={<AddBook />}/>
+          <Route path="/Search" element={<BookSearch/>}/>
         </Routes>
       </BrowserRouter>
     </div>
