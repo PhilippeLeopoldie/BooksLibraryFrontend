85cfc8a6f524b2982a983d4e5000b35890e69764
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-17 11:02:28 +0200

    Implement BookList test 'Loading... 'message checking

--- frontend/LibraryFrontEnd/src/Component/BookList/BookList.test.tsx
+++ frontend/LibraryFrontEnd/src/Component/BookList/BookList.test.tsx
@@ -1,27 +1,49 @@
 import { BookList } from "./BookList";
-import { act, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { useState } from "react";
 
-global.fetch = jest.fn().mockImplementation(() =>
-  Promise.resolve({
-    json: () =>
-      Promise.resolve([
-        { book: { id: 1, title: "Book 1", author: "Author 1" } },
-      ]),
-    status: 200,
-    hearders: new Headers(),
-  })
-);
 
-test("Should render Loading message initially", async () => {
-  //Arrange
- await act( async () => {
-  render(<BookList />);
- }) 
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  useState: jest.fn(),
+}));
+describe("BookList 'Loading...'message checking", () => {
+  test("renders 'Loading...' when books state is null", () => {
+    // arange
+    (useState as jest.Mock).mockImplementation(() => [null, jest.fn()]);
 
-  //Act
-  const loadingMessage = screen.getByText("Loading...");
+    // Act
+    render(<BookList />);
+    const loadingMessage = screen.getByText("Loading...");
 
-  //Expect
-  expect(loadingMessage).toBeInTheDocument();
+    // expect
+    expect(loadingMessage).toBeInTheDocument();
+  });
+
+  test("should not render 'Loading...' when books state is not null", () => {
+    // arrange
+    type BookType = {
+      book: {
+        id: number;
+        title: string;
+        author: string;
+      };
+    };
+    const book: BookType = {
+      book: {
+        id: 1,
+        title: "title 1",
+        author: "author 1",
+      },
+    };
+    (useState as jest.Mock).mockImplementation(() => [book, jest.fn()]);
+
+    // Act
+    render(<BookList />);
+    const loadingMessage = screen.queryByText("Loading...");
+
+    // expect
+    expect(loadingMessage).toBeNull();
+  });
 });
