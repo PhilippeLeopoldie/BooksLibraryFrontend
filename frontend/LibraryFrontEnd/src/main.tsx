e80a3799facce24543366508638b9d4bb0adc8ad
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-18 01:47:30 +0200

    Refactor: reorganise imports in main.tsx

--- frontend/LibraryFrontEnd/src/main.tsx
+++ frontend/LibraryFrontEnd/src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './Component/App/App'
-import './index.css'
 import './Component/OpinionEdit/OpinionEdit.css'
 import './Component/Book/Book.css'
 import './Component/Opinion/Opinion.css'
 import './Component/BookCreate/BookCreate.css'
-
+import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
