492e45979ed7dcd18af9d7a352c50eb3d2986218
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-19 13:20:20 +0200

    Add input search field and pass its values to Booklist in BookSearch

--- frontend/LibraryFrontEnd/src/Component/BookSearch/BookSearch.tsx
+++ frontend/LibraryFrontEnd/src/Component/BookSearch/BookSearch.tsx
@@ -1,12 +1,44 @@
+import { useState } from "react"
 import { BookList } from "../BookList/BookList"
 
+type BooksSearchCriteria = {
+  search?:{
+    title: string,
+    author: string
+  }
+}
+
 export const BookSearch = () => {
+  const [searchCriteria, setSearchCriteria] = useState<BooksSearchCriteria>({
+    search:{
+      title: "",
+      author: ""
+    }
+  })
+  const [inputValue,setInputValue] = useState<string>("");
+  
   return (
-    <div>
-      search page
-      <BookList/>
-      
-    </div>
+    <>
+      <form className="searchForm">
+        <input
+          className="searchForm__input"
+          autoFocus
+          placeholder="Search by Title or Author"
+          name="titleOrAuthor"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+            setSearchCriteria({
+               search:{
+                title:e.target.value,
+                author: e.target.value
+              }
+            })
+          }}
+        />
+      </form>
+      <BookList searchCriteria={searchCriteria.search}/>
+    </>
   )
 }
 
