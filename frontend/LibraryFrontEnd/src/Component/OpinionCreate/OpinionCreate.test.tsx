5d3e767c080f7df745da5798515cdf37e3009068
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-20 21:43:20 +0200

    Add test snapShot to OpinionCreate.test

---
+++ frontend/LibraryFrontEnd/src/Component/OpinionCreate/OpinionCreate.test.tsx
@@ -0,0 +1,11 @@
+import {render, screen,} from "@testing-library/react";
+import { OpinionCreate } from "./OpinionCreate";
+import "@testing-library/jest-dom";
+
+describe("OpinionCreate", () => {
+  test("snapShot: render Opinioncreate", ()=> {
+    const {asFragment} = render(<OpinionCreate toCreate={()=> {}}/>)
+
+    expect(asFragment()).toMatchSnapshot();
+  })
+})
