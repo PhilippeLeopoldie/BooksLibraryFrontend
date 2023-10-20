b0b6707e86f43077d95a606443fb4b42a808656f
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-20 21:32:17 +0200

    Add test snapShot and reorganize tests in BookList.test

--- frontend/LibraryFrontEnd/src/Component/BookList/BookList.test.tsx
+++ frontend/LibraryFrontEnd/src/Component/BookList/BookList.test.tsx
@@ -8,7 +8,29 @@ jest.mock("react", () => ({
   ...jest.requireActual("react"),
   useState: jest.fn(),
 }));
-describe("BookList 'Loading...'message checking", () => {
+
+type BookType = {
+  book: {
+    id: number;
+    title: string;
+    author: string;
+  };
+};
+const book: BookType = {
+  book: {
+    id: 1,
+    title: "title 1",
+    author: "author 1",
+  },
+};
+describe("BookList rendering", () => {
+  test("snapShot: should render BookList", () => {
+    (useState as jest.Mock).mockImplementation(() => [book, jest.fn()]);
+    const {asFragment} = render(<BookList/>)
+
+    expect(asFragment()).toMatchSnapshot();
+  })
+
   test("renders 'Loading...' when books state is null", () => {
     // arange
     (useState as jest.Mock).mockImplementation(() => [null, jest.fn()]);
@@ -23,20 +45,7 @@ describe("BookList 'Loading...'message checking", () => {
 
   test("should not render 'Loading...' when books state is not null", () => {
     // arrange
-    type BookType = {
-      book: {
-        id: number;
-        title: string;
-        author: string;
-      };
-    };
-    const book: BookType = {
-      book: {
-        id: 1,
-        title: "title 1",
-        author: "author 1",
-      },
-    };
+   
     (useState as jest.Mock).mockImplementation(() => [book, jest.fn()]);
 
     // Act
