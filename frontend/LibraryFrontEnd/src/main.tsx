ac0fc896996ab1874aca640d1c4981c3297c1041
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-21 02:44:38 +0200

    Move CSS imports to their related component folder

--- frontend/LibraryFrontEnd/src/main.tsx
+++ frontend/LibraryFrontEnd/src/main.tsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './Component/App/App'
-import './Component/OpinionEdit/OpinionEdit.css'
-import './Component/Book/Book.css'
-import './Component/Opinion/Opinion.css'
-import './Component/BookCreate/BookCreate.css'
-import './index.css'
-
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
