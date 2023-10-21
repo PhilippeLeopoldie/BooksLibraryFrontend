1873b29f756d96fd012959e511e8cc8ef5f175b8
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-21 02:20:02 +0200

    Import CSS file in OpinionEdit.tsx

--- frontend/LibraryFrontEnd/src/Component/OpinionEdit/OpinionEdit.tsx
+++ frontend/LibraryFrontEnd/src/Component/OpinionEdit/OpinionEdit.tsx
@@ -1,6 +1,7 @@
 import { SyntheticEvent, useState, useContext } from "react";
 import url from "../../Url";
 import { RateClick } from "./RateClick/RateClick";
+import "./OpinionEdit.css"
 
 type EditedOpinion = {
   id:number
