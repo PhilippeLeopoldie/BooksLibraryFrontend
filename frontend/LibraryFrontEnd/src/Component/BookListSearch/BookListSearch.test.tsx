38620970b4fbd7bf21cd752c8c098cb79ed97aac
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-23 01:03:18 +0200

    Test: Implement BookListSearch.test

--- frontend/LibraryFrontEnd/src/Component/BookListSearch/BookListSearch.test.tsx
+++ frontend/LibraryFrontEnd/src/Component/BookListSearch/BookListSearch.test.tsx
@@ -1,9 +1,44 @@
 import {render, screen,} from "@testing-library/react";
 import { BookListSearch } from "./BookListSearch";
 import "@testing-library/jest-dom";
+import { useState } from "react";
+
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  useState: jest.fn(),
+}));
+
+type BooksType = {
+  book: {
+    id: number;
+    title: string;
+    author: string;
+  };
+};
+
+const book : BooksType = {
+  book:{
+    id: 1,
+    title: "title",
+    author: "author",
+  }
+
+}
 
 describe("BookListSearch", () => {
   test("snapShot: render BookListSearch", ()=> {
+    (useState as jest.Mock).mockImplementation(() => [book, jest.fn()]);
+    (useState as jest.Mock).mockImplementation(() => [book, jest.fn()]);
+
+    const {asFragment} = render(<BookListSearch/>)
+
+    expect(asFragment()).toMatchSnapshot();
+  })
+
+  test("snapShot: render 'Loading...'", ()=> {
+    (useState as jest.Mock).mockImplementation(() => [null, jest.fn()]);
+    (useState as jest.Mock).mockImplementation(() => [book, jest.fn()]);
+
     const {asFragment} = render(<BookListSearch/>)
 
     expect(asFragment()).toMatchSnapshot();
