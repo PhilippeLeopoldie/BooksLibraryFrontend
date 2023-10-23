121ee805018c6b30ac0f1f6f206d509a6804d59b
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-23 13:03:51 +0200

    Refactor: delete empty space & update indentation in Opinion.tsx

--- frontend/LibraryFrontEnd/src/Component/Opinion/Opinion.tsx
+++ frontend/LibraryFrontEnd/src/Component/Opinion/Opinion.tsx
@@ -1,63 +1,63 @@
 import { useEffect, useState, useContext } from "react";
-import "./Opinion.css"
+import "./Opinion.css";
 //import { OpinionType } from "../../Type";
 import url from "../../Url";
 import modify from "../../media/write.svg";
 import { Rate } from "./Rate/Rate";
 
-
-
-
 type OpinionType = {
-  id: number,
-  rate: number,
-  view: string,
-  userName: string,
-  postDate: string,
-  bookId: number
+  id: number;
+  rate: number;
+  view: string;
+  userName: string;
+  postDate: string;
+  bookId: number;
 };
 
 type BookType = {
   book: {
-    id:number,
-    title:string,
-    author:string
-  }
-  onEdit?: (opinionToUpdate:OpinionType) => void
-  toCreate: ()=> void
+    id: number;
+    title: string;
+    author: string;
+  };
+  onEdit?: (opinionToUpdate: OpinionType) => void;
+  toCreate: () => void;
 };
 
-
-export const Opinion = ({book, onEdit, toCreate}: BookType) => {
-  const [opinions, setOpinions] = useState<OpinionType[] | null >(null);
+export const Opinion = ({ book, onEdit, toCreate }: BookType) => {
+  const [opinions, setOpinions] = useState<OpinionType[] | null>(null);
   const [fetching, setFething] = useState<boolean>(true);
   const [lastOpinion, setLastOpinion] = useState<OpinionType | null>(null);
-  const FetchOpinions = async (bookId :number) => {
-   try {
-    const response : Response = await fetch(url + "api/Opinion/BookId=" + bookId);
-    if(response.status === 200) {
-      const responseData = await response.json();
-        setOpinions(responseData.$values)
-        setFething(false)
-    } else if (response.status === 404){
-      setFething(false)
-      console.log("404 response",response)
+  const FetchOpinions = async (bookId: number) => {
+    try {
+      const response: Response = await fetch(
+        url + "api/Opinion/BookId=" + bookId
+      );
+      if (response.status === 200) {
+        const responseData = await response.json();
+        setOpinions(responseData.$values);
+        setFething(false);
+      } else if (response.status === 404) {
+        setFething(false);
+        console.log("404 response", response);
+      }
+    } catch (error) {
+      console.error("error fetching Opinions:", error);
     }
-   } catch (error) {
-    console.error("error fetching Opinions:", error);
-   }
-  } 
+  };
 
   useEffect(() => {
     FetchOpinions(book.id);
   }, []);
 
-  useEffect(()=> {
-    opinions && opinions.length > 0 && setLastOpinion(opinions[opinions.length-1])
-  },[opinions])
-  
-  if(fetching) {
-    return <h2 className="OpinionLoading">Loading...</h2>
+  useEffect(() => {
+    opinions &&
+      opinions.length > 0 &&
+      setLastOpinion(opinions[opinions.length - 1]);
+  }, [opinions]);
+
+  if (fetching) {
+    return <h2 className="OpinionLoading">Loading...</h2>;
   }
 
   return (
@@ -65,20 +65,25 @@ export const Opinion = ({book, onEdit, toCreate}: BookType) => {
       <div className="opinioncontainer">
         {lastOpinion && (
           <div className="opinioncontainer--card" key={lastOpinion.id}>
-            <div className="opinionCardItems" >
-              <p className="opinionCardItems opinioncard__nbReview">{opinions?.length} reviews</p>
+            <div className="opinionCardItems">
+              <p className="opinionCardItems opinioncard__nbReview">
+                {opinions?.length} reviews
+              </p>
               <textarea
                 className="opinionCardItems opinioncard--view"
-                value={lastOpinion.view.length > 84 ? 
-                  `${lastOpinion.view.slice(0, 84)}...`
-                  :
-                  lastOpinion.view}
+                value={
+                  lastOpinion.view.length > 84
+                    ? `${lastOpinion.view.slice(0, 84)}...`
+                    : lastOpinion.view
+                }
                 readOnly
               />
               <div className="opinionCardItems">{lastOpinion.userName}</div>
               <div className="opinionCardItems opinioncard__rate">
-                <Rate rate= {lastOpinion.rate}/>
-                <p className="opinionCardItems opinioncard__postDate" >{lastOpinion.postDate}</p>
+                <Rate rate={lastOpinion.rate} />
+                <p className="opinionCardItems opinioncard__postDate">
+                  {lastOpinion.postDate}
+                </p>
               </div>
             </div>
           </div>
@@ -86,4 +91,4 @@ export const Opinion = ({book, onEdit, toCreate}: BookType) => {
       </div>
     </>
   );
-}
+};
