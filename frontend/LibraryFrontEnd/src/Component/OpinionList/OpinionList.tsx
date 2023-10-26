2280bc8101efa7cbcf8c38bba869e29d1b7ee270
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-10-26 17:18:34 +0200

    Implement OpinionList

---
+++ frontend/LibraryFrontEnd/src/Component/OpinionList/OpinionList.tsx
@@ -0,0 +1,59 @@
+import { Rate } from "../Opinion/Rate/Rate";
+import "./OpinionList.css";
+
+type Review = {
+  id: number;
+  rate: number;
+  view: string;
+  userName: string;
+  postDate: string;
+  bookId: number;
+};
+
+type DisplayReview = () => void;
+
+type Book = {
+  id: number;
+  title: string;
+  author: string;
+};
+
+export const OpinionList = ({
+  opinions,
+  displayReviews,
+  book,
+}: {
+  opinions: Review[] | undefined;
+  displayReviews: DisplayReview;
+  book: Book | undefined;
+}) => {
+  return (
+    <>
+      <div className="OpinionList OpinionList__container">
+        <button onClick={() => displayReviews()}>CLOSE</button>
+        <div className="OpinionList OpinionList__header">
+          <h1 className="OpinionList">{book?.title}</h1>
+          <h3 className="OpinionList">Author:{book?.author}</h3>
+        </div>
+        <div className="OpinionList__filter"></div>
+        <div className=" OpinionList OpinionList__reviews">
+          {opinions?.map((review, index) => (
+            <div className="OpinionList" key={index}>
+              <p className="OpinionList opinionlist__userName">
+                {review.userName}
+              </p>
+              <div className="OpinionList OpinionList__rate--flex">
+                <Rate rate={review.rate} />
+                <p className="OpinionList OpinionList__date">
+                  {review.postDate}
+                </p>
+              </div>
+              <p className="OpinionList OpinionList__view">{review.view}</p>
+              <hr className="OpinionList OpinionList__horizontalLine" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
