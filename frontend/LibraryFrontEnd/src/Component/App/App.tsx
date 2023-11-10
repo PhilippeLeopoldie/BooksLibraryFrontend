fa0398376966a451a82068a7690ec1f543ae4876
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-11-10 21:04:43 +0100

    Reorganize nav bar structure in App.tsx and App.css

--- frontend/LibraryFrontEnd/src/Component/App/App.tsx
+++ frontend/LibraryFrontEnd/src/Component/App/App.tsx
@@ -3,64 +3,52 @@ import { BookCreate } from "../BookCreate/BookCreate";
 import { BookList } from "../BookList/BookList";
 import { Routes, BrowserRouter, Route, Link } from "react-router-dom";
 import { BookSearch } from "../BookSearch/BookSearch";
-import addIcone from "../../media/add64px.svg";
-import bookIcone from "../../media/book64px.svg";
-import homeIcone from "../../media/home_42px.svg";
-import searchIcone from "../../media/search64px.svg";
+import addIcone from "../../media/add20px.svg";
+import bookIcone from "../../media/book48px.svg";
+import homeIcone from "../../media/home_48px.svg";
+import searchIcone from "../../media/search48px.svg";
 
 export const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="navbar--flex">
-          <div className="navElement navbar__home">
-            <Link className="nav-link homeLink" to="/">
-              <img className="nav-link nav__HomeIcone" src={homeIcone} alt="Home"></img>
-              <figcaption className="iconeTitle nav__Home__title">
-                Home
-              </figcaption>
-            </Link>
-          </div>
-          <div className="navElement navbar__search">
-            <Link className="nav-link" to="/Search">
-              <img
-                className="nav-link nav__search"
-                width="48"
-                height="48"
-                src={searchIcone}
-                alt="Search"
-              />
-              <figcaption className="iconeTitle nav__search__title">
-                Search
-              </figcaption>
-            </Link>
-          </div>
-          <div className="navElement navbar__addBook">
-            <Link className="nav-link addBook" to="/addBook">
-              <img
-                className="nav-link nav__addBook"
-                width="48"
-                height="48"
-                src={bookIcone}
-                alt="book"
-              />
-              <img
-                className="nav-link addIcone"
-                width="20"
-                height="20"
-                src={addIcone}
-                alt="add"
-              />
-              <figcaption className="iconeTitle nav__AddBook__Title">
-                Add a book
-              </figcaption>
-            </Link>
-          </div>
+        <div className="navbar--grid">
+          <Link className="nav-link nav__home__Link--flex" to="/">
+            <img
+              className="nav-link icone nav__HomeIcone"
+              src={homeIcone}
+              alt="Home"
+            ></img>
+            <figcaption className="iconeTitle nav__Home__title">
+              Home
+            </figcaption>
+          </Link>
+          <Link className="nav-link nav__search__link--flex" to="/Search">
+            <img
+              className="nav-link icone nav__search__image"
+              src={searchIcone}
+              alt="Search"
+            />
+            <figcaption className="iconeTitle nav__search__title">
+              Search
+            </figcaption>
+          </Link>
+          <Link className="nav-link nav__addBook--flex" to="/addBook">
+            <img
+              className="nav-link icone nav__addBook"
+              src={bookIcone}
+              alt="book"
+            />
+            <img className="nav-link addIcone" src={addIcone} alt="add" />
+            <figcaption className="iconeTitle nav__AddBook__Title">
+              Add a book
+            </figcaption>
+          </Link>
         </div>
         <Routes>
-          <Route path="/" element={<BookList />}/>
-          <Route path="/addBook" element={<BookCreate />}/>
-          <Route path="/Search" element={<BookSearch/>}/>
+          <Route path="/" element={<BookList />} />
+          <Route path="/addBook" element={<BookCreate />} />
+          <Route path="/Search" element={<BookSearch />} />
         </Routes>
       </BrowserRouter>
     </div>
