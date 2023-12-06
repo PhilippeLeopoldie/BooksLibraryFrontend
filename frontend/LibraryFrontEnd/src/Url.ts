53f88105e1cdba5a53c208a0182ac78b8a584650
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-12-06 16:40:21 +0100

    Switch book and opinion endpoints to their plural forms in Url.ts

--- frontend/LibraryFrontEnd/src/Url.ts
+++ frontend/LibraryFrontEnd/src/Url.ts
@@ -1,10 +1,10 @@
 export const API_URL : string = "https://booklibrary-backend-20f7a19cecb2.herokuapp.com/api/" 
 //export const API_URL = "https://localhost:7152/api/";
-export const BOOK_URL : string = API_URL+"Book";
+export const BOOK_URL : string = API_URL+"Books";
 export const BOOK_BY_TITLE_OR_AUTHOR_URL : string = BOOK_URL+"/TitleOrAuthor/";
-export const OPINION_URL: string = API_URL+"Opinion/";
-export const OPINION_BY_BOOKID_URL : string = API_URL+"Opinion/BookId=";
-
+export const OPINION_URL: string = API_URL+"Opinions/";
+export const OPINION_BY_BOOKID_URL : string = OPINION_URL+"BookId=";
+ 
 
 
 
