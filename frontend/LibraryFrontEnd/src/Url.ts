5c892c2f871a92f0f71915dda2665594a7853ef4
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-12-17 15:39:59 +0100

    update default environment url

--- frontend/LibraryFrontEnd/src/Url.ts
+++ frontend/LibraryFrontEnd/src/Url.ts
@@ -1,5 +1,5 @@
-//export const API_URL : string = "https://booklibrary-backend-20f7a19cecb2.herokuapp.com/api/" 
-export const API_URL : string = "https://localhost:7152/api/";
+export const API_URL : string = "https://booklibrary-backend-20f7a19cecb2.herokuapp.com/api/" 
+//export const API_URL : string = "https://localhost:7152/api/";
 export const BOOK_URL : string = API_URL+"Books";
 export const BOOK_BY_BOOKID_URL: string = BOOK_URL+"/";
 export const BOOK_BY_TITLE_OR_AUTHOR_URL : string = BOOK_URL+"/TitleOrAuthor/";
