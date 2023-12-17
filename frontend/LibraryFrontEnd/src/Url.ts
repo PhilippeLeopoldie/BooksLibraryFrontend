88a888827471329ea30028ac650ff270b2578338
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-12-17 15:33:42 +0100

    Fix typo in Url.ts

--- frontend/LibraryFrontEnd/src/Url.ts
+++ frontend/LibraryFrontEnd/src/Url.ts
@@ -1,6 +1,7 @@
-export const API_URL : string = "https://booklibrary-backend-20f7a19cecb2.herokuapp.com/api/" 
-//export const API_URL = "https://localhost:7152/api/";
+//export const API_URL : string = "https://booklibrary-backend-20f7a19cecb2.herokuapp.com/api/" 
+export const API_URL : string = "https://localhost:7152/api/";
 export const BOOK_URL : string = API_URL+"Books";
+export const BOOK_BY_BOOKID_URL: string = BOOK_URL+"/";
 export const BOOK_BY_TITLE_OR_AUTHOR_URL : string = BOOK_URL+"/TitleOrAuthor/";
 export const OPINION_URL: string = API_URL+"Opinions/";
 export const OPINION_BY_BOOKID_URL : string = OPINION_URL+"BookId=";
