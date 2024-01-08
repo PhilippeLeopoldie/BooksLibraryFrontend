00fae55208680e297f7a2a83dac505bfc57e8078
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-01-08 15:13:40 +0100

    Update BookByTitleOrAuthor URL in Url.ts

--- frontend/LibraryFrontEnd/src/Url.ts
+++ frontend/LibraryFrontEnd/src/Url.ts
@@ -3,7 +3,7 @@ export const API_URL : string = "https://booklibrary-backend-20f7a19cecb2.heroku
 export const BOOK_URL : string = API_URL+"Books";
 export const BOOK_TOP_BOOK_URL : string = BOOK_URL+"/TopBooks?numberOfBooks="
 export const BOOK_BY_BOOKID_URL: string = BOOK_URL+"/";
-export const BOOK_BY_TITLE_OR_AUTHOR_URL : string = BOOK_URL+"/TitleOrAuthor/";
+export const BOOK_BY_TITLE_OR_AUTHOR_URL : string = BOOK_URL+"/TitleOrAuthor?titleOrAuthor=";
 export const OPINION_URL: string = API_URL+"Opinions/";
 export const OPINION_BY_BOOKID_URL : string = OPINION_URL+"BookId=";
  
