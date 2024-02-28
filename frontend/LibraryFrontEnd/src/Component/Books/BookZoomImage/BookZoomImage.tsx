31e6b8caad4991b2a4db465046bae1565604df58
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-02-28 17:43:44 +0100

    Implemente BookZoomImage component

---
+++ frontend/LibraryFrontEnd/src/Component/Books/BookZoomImage/BookZoomImage.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+type BookImage = {
+  imageUrl?: string;
+  zoom: string
+}
+
+export const BookZoomImage = ({imageUrl,zoom}:BookImage) => {
+  const [imageZoom, setImageZoom] = useState<string>(zoom);
+  const toggleZoomImage = () => {
+    imageZoom === "zoomOut" ? setImageZoom("zoomIn") : setImageZoom("zoomOut");
+  };
+
+  return (
+    <div>
+      <img
+            src={imageUrl && `${imageUrl}`}
+            className={`boocard__Image-${imageZoom} boocard__Image`}
+            alt="bookImage"
+            width="576px"
+            height="652px"
+            onClick={toggleZoomImage}
+          />
+    </div>
+  )
+}
