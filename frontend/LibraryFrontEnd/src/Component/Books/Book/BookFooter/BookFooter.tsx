21417b1d0e27dd03b1e28ff3f08bc03fb08bab0b
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-03-08 14:59:21 +0100

    Implement a bookFooter component

---
+++ frontend/LibraryFrontEnd/src/Component/Books/Book/BookFooter/BookFooter.tsx
@@ -0,0 +1,79 @@
+import  { useContext } from 'react'
+import { Opinion } from '../../../Opinions/Opinion/Opinion';
+import { ThemeContext } from '../../../App/App';
+
+type BookFooterType = {
+  bookFooter: {
+    toggleCreateOpinion: (view:string) => void,
+    handleOpinionList: (reviews: Reviews[]) => void,
+    handleAverageClick: () => void,
+    averageClick: boolean,
+  },
+  updatedBook : bookType
+}
+
+type bookType = {
+    id: number;
+    title: string;
+    author: string;
+    averageRate: number;
+    imageUrl?: string;
+}
+
+type Reviews = {
+  id: number;
+  rate: number;
+  view: string;
+  userName: string;
+  postDate: string;
+  bookId: number;
+};
+
+
+export const BookFooter = ({bookFooter,updatedBook}:BookFooterType) => {
+
+  const theme = useContext(ThemeContext);
+  
+  return (
+    <>
+    <footer className="bookcard__footer--flex">
+          <div className="bookcard__footer-average-rate--flex">
+            {!bookFooter.averageClick ? (
+              <div className="bookcard__footer-average-rate--flex">
+                <a
+                  className="bookcard__footer-average-rate"
+                  onClick={() => {
+                    bookFooter.handleAverageClick();
+                  }}
+                >
+                  {updatedBook.averageRate}/5
+                </a>
+                <div className="rate_star"> &#9733;</div>
+              </div>
+            ) : (
+              <h4 className={`opinion__loading opinion__loading--${theme}`}>
+                Loading...
+              </h4>
+            )}
+          </div>
+          {bookFooter.averageClick && (
+            <Opinion
+              book={updatedBook}
+              displayReview={bookFooter.handleOpinionList}
+            />
+          )}
+          <button
+            className="button bookFooter__RateButton"
+            onClick={() => {
+              bookFooter.toggleCreateOpinion("createOpinion");
+            }}
+          >
+            Rate this book
+          </button>
+        </footer>
+      
+    </>
+  )
+}
+
+
