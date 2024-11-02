2920d6af902d443f786e5582abc9567b660a9a98
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-11-02 13:26:03 +0100

    feat: Implement debouncing for book search; improve readability; and separation of concerns in CSS files

--- frontend/LibraryFrontEnd/src/Component/BookSearch/BookSearchList/BookSearchList.tsx
+++ frontend/LibraryFrontEnd/src/Component/BookSearch/BookSearchList/BookSearchList.tsx
@@ -1,84 +1,89 @@
 import { BookCard } from "../../BookCard/BookCard";
 import "../../BookSearch/BookSearchList/BookSearchList.css";
 import { useContext, useEffect, useState } from "react";
-import {BOOK_BY_TITLE_OR_AUTHOR_URL} from "../../../Url";
+import { BOOK_BY_TITLE_OR_AUTHOR_URL } from "../../../Url";
 import { ThemeContext } from "../../App/App";
 
 type BooksType = {
-  book: {
-    id: number;
-    title: string;
-    author: string;
-    averageRate: number;
-  };
+    book: {
+        id: number;
+        title: string;
+        author: string;
+        averageRate: number;
+    };
 };
 
 type BooksSearchCriteria = {
-  titleOrAuthor: string;
+    titleOrAuthor: string;
 };
 
 export const BookSearchList = ({ titleOrAuthor }: BooksSearchCriteria) => {
-  
-  const [books, setBooks] = useState<BooksType[] | null>([]);
-  const [loading, setLoading] = useState<string>("Loading...");
-  const theme = useContext(ThemeContext);
-  const fetchBooksByTitleOrAuthor = async () => {
-    try {
-      if (titleOrAuthor === "") {
-        setBooks([]);
-        setLoading("Loading...");
-      }
-      if (titleOrAuthor !== "") {
-        const booksResponse: Response = await fetch(
-          BOOK_BY_TITLE_OR_AUTHOR_URL + titleOrAuthor
-        );
-        if (booksResponse.status === 200) {
-          const booksResponseData = await booksResponse.json();
-          setBooks(booksResponseData);
-          setLoading("");
-        } else if (booksResponse.status === 404) {
-          setBooks([]);
-          setLoading("");
-          console.log(booksResponse);
+
+    const [books, setBooks] = useState<BooksType[] | null>([]);
+    const searching = "Searching ...";
+    const [waitingMessage, setWaitingMessage] = useState<string>(searching);
+    const theme = useContext(ThemeContext);
+    const fetchBooksByTitleOrAuthor = async () => {
+        try {
+            if (titleOrAuthor === "") {
+                setBooks([]);
+                setWaitingMessage(searching);
+            }
+            if (titleOrAuthor !== "") {
+                const booksResponse: Response = await fetch(
+                    BOOK_BY_TITLE_OR_AUTHOR_URL + titleOrAuthor
+                );
+                if (booksResponse.status === 200) {
+                    const booksResponseData = await booksResponse.json();
+                    setBooks(booksResponseData);
+                    setWaitingMessage("");
+                } else if (booksResponse.status === 404) {
+                    setBooks([]);
+                    setWaitingMessage("");
+                    console.log(booksResponse);
+                }
+            }
+        } catch (error) {
+            console.error("Error fetching books:", error);
         }
-      }
-    } catch (error) {
-      console.error("Error fetching books:", error);
+    };
+
+    useEffect(() => {
+        setWaitingMessage(searching);
+        const debounceTimer = setTimeout(() => {
+            fetchBooksByTitleOrAuthor();
+        }, 2000);
+        return () => clearTimeout(debounceTimer);
+    }, [titleOrAuthor]);
+
+    if (titleOrAuthor === "") {
+        return <h1 className={"bookSearchListContainer--" + theme}></h1>;
     }
-  };
+    if (titleOrAuthor != "" && waitingMessage) {
+        return <h2 className={"bookSearchListContainer--" + theme}>{waitingMessage}</h2>;
+    }
 
-  useEffect(() => {
-    fetchBooksByTitleOrAuthor();
-  }, [titleOrAuthor]);
-
-  if (titleOrAuthor==="") {
-    return <h1 className={"bookSearchListContainer--" + theme}></h1>;
-  }
-  if (titleOrAuthor !="" && loading) {
-      return <h1 className={"bookSearchListContainer--" + theme}>{loading}</h1>;
-  }
-
-  return (
-    <>
-       <div className={"bookSearchListContainer--" + theme}>
-        <h2>
-          {books && books.length > 1
-            ? `(${books.length}) books found`
-            : `(${books?.length}) book found`}
-        </h2>
-        <div className={books && books.length > 1 ? "bookListContainer" :"bookListContainer-center"}>
-          {books &&
-            Array.isArray(books) &&
-            books
-              .sort((a, b) => b.book.id - a.book.id)
-              .map((bookDetail, index) => (
-                <BookCard
-                  key={bookDetail.book.id}
-                  book={bookDetail.book}
-                />
-              ))}
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={"bookSearchListContainer--" + theme}>
+                <h2>
+                    {books && books.length > 1
+                        ? `(${books.length}) books found`
+                        : `(${books?.length}) book found`}
+                </h2>
+                <div className={books && books.length > 1 ? "bookSearchListContainer" : "bookSearchListContainer--center"}>
+                    {books &&
+                        Array.isArray(books) &&
+                        books
+                            .sort((a, b) => b.book.id - a.book.id)
+                            .map((bookDetail) => (
+                                <BookCard
+                                    key={bookDetail.book.id}
+                                    book={bookDetail.book}
+                                />
+                            ))}
+                </div>
+            </div>
+        </>
+    );
 };
