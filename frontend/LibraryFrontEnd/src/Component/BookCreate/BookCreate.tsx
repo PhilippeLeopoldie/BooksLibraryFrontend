421a438e91ca6906151fc55a1cff9daf8343313d
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-11-12 15:34:35 +0100

    refactor: Define sections for improved organization and correct code indentation for readability

--- frontend/LibraryFrontEnd/src/Component/BookCreate/BookCreate.tsx
+++ frontend/LibraryFrontEnd/src/Component/BookCreate/BookCreate.tsx
@@ -1,172 +1,170 @@
 import { useContext, useState } from "react";
 import "./BookCreate.css";
 import { Link } from "react-router-dom";
-import {BOOK_URL} from "../../Url";
+import { BOOK_URL } from "../../Url";
 import { ThemeContext } from "../App/App";
 
 type FormDataType = {
-  title: string;
-  author: string;
-  imageUrl: string;
-};
-
-type BookType = {
-  book: {
-    id: number;
     title: string;
     author: string;
     imageUrl: string;
-  };
+};
+
+type BookType = {
+    book: {
+        id: number;
+        title: string;
+        author: string;
+        imageUrl: string;
+    };
 };
 
 export const BookCreate = () => {
-  const theme = useContext(ThemeContext);
-  const [formData, setFormData] = useState<FormDataType>({
-    title: "",
-    author: "",
-    imageUrl: "",
-  });
-
-  const [books, setBooks] = useState<BookType[]>([]);
-  const [bookCreatedMessage, setBookCreatedMessage] = useState<boolean>(false);
-  const [errorBook, setErrorBook] = useState<Boolean>(false);
-  const [errorBookDetail, setErrorBookDetail] = useState<string>("");
-  const [newBookId, setNewBookId] = useState<number>(0);
-  const rootElement = document.documentElement;
-  const PostBook = async () => {
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title: formData.title,
-        author: formData.author,
-        imageUrl: formData.imageUrl,
-      }),
-    };
-    const bookResponse: Response = await fetch(
-      BOOK_URL,
-      requestOptions
-    );
-    if (bookResponse.status === 201) {
-      setErrorBook(false);
-      const body = await bookResponse.json();
-      setNewBookId(body.id);
-      setBooks((books) => [...books, body]);
-      setBookCreatedMessage(true);
-      setFormData({
+    const theme = useContext(ThemeContext);
+    const [formData, setFormData] = useState<FormDataType>({
         title: "",
         author: "",
         imageUrl: "",
-      });
-      return body;
-    } else if (bookResponse.status === 400) {
-      setErrorBook(true);
-      const errorData = await bookResponse.json();
-      setErrorBookDetail(errorData.detail);
-    }
-  };
-  const HideBookCreatedMessage = () => {
-    setBookCreatedMessage(false);
-  };
+    });
 
-  const HideErrorDetail = () => {
-    setErrorBook(false);
-  };
-
-  const HandleInputChange = (
-    e:
-      | React.ChangeEvent<HTMLInputElement>
-      | React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-    bookCreatedMessage && HideBookCreatedMessage();
-    errorBook && HideErrorDetail();
-  };
-
-  return (
-    <>
-      <div
-        className={`book-create-form__container--${theme} book-create-form__container`}
-      >
-        <script>
-          {theme === "black"
-            ? (rootElement.style.backgroundColor = "#000000")
-            : (rootElement.style.backgroundColor = "#f3f3f4")}
-        </script>
-        <form
-          onSubmit={(e) => e.preventDefault()}
-          action={BOOK_URL}
-          method="POST"
-          className="book-create-form"
-        >
-          <input
-            className={`input input--${theme}`}
-            placeholder="Title"
-            name="title"
-            value={formData.title}
-            onChange={(e) => HandleInputChange(e)}
-          />
-
-          <input
-            className={`input input--${theme}`}
-            placeholder="Author"
-            name="author"
-            value={formData.author}
-            onChange={(e) => HandleInputChange(e)}
-          />
-
-          <input
-            className={`input input--${theme} input__imageUrl--${theme}`}
-            placeholder="Book Image link"
-            name="imageUrl"
-            value={formData.imageUrl}
-            onChange={(e) => HandleInputChange(e)}
-          />
-
-          <div className="book-create-form__output-card">
-            {errorBook && (
-              <div className="validation__error-message">{errorBookDetail}</div>
-            )}
-            {bookCreatedMessage && (
-            <div className="book-create-form__output-card">
-              <div className="book-create-form__output">Book created!</div>
-              <Link to="/">
-                <h3 className={`book-create-form__output--back-to-home--${theme}`}>
-                  back to Home
-                </h3>
-              </Link>
-            </div>
-          )}
-          </div>
-          <footer className="footer--flex">
-            <button
-            className="button book-create-form__cancel-button"
-            onClick={()=>{
-              setFormData({
+    const [books, setBooks] = useState<BookType[]>([]);
+    const [bookCreatedMessage, setBookCreatedMessage] = useState<boolean>(false);
+    const [errorBook, setErrorBook] = useState<Boolean>(false);
+    const [errorBookDetail, setErrorBookDetail] = useState<string>("");
+    const [newBookId, setNewBookId] = useState<number>(0);
+    const rootElement = document.documentElement;
+    const PostBook = async () => {
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: formData.title,
+                author: formData.author,
+                imageUrl: formData.imageUrl,
+            }),
+        };
+        const bookResponse: Response = await fetch(
+            BOOK_URL,
+            requestOptions
+        );
+        if (bookResponse.status === 201) {
+            setErrorBook(false);
+            const body = await bookResponse.json();
+            setNewBookId(body.id);
+            setBooks((books) => [...books, body]);
+            setBookCreatedMessage(true);
+            setFormData({
                 title: "",
                 author: "",
                 imageUrl: "",
-              });
-            }}
-            > 
-            Cancel
-            </button>
-            <button
-            className="button book-create-form__post-button"
-            onClick={async () => {
-              await PostBook();
-            }}
-            >
-            Create book
-          </button>
+            });
+            return body;
+        } else if (bookResponse.status === 400) {
+            setErrorBook(true);
+            const errorData = await bookResponse.json();
+            setErrorBookDetail(errorData.detail);
+        }
+    };
+    const HideBookCreatedMessage = () => {
+        setBookCreatedMessage(false);
+    };
 
-          </footer>
-          
-          
-        </form>
-      </div>
-    </>
-  );
+    const HideErrorDetail = () => {
+        setErrorBook(false);
+    };
+
+    const HandleInputChange = (
+        e:
+            | React.ChangeEvent<HTMLInputElement>
+            | React.ChangeEvent<HTMLTextAreaElement>
+    ) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+        bookCreatedMessage && HideBookCreatedMessage();
+        errorBook && HideErrorDetail();
+    };
+
+    return (
+        <>
+            <section
+                className={`book-create-form__container--${theme} book-create-form__container`}
+            >
+                <script>
+                    {theme === "black"
+                        ? (rootElement.style.backgroundColor = "#000000")
+                        : (rootElement.style.backgroundColor = "#f3f3f4")}
+                </script>
+                <form
+                    onSubmit={(e) => e.preventDefault()}
+                    action={BOOK_URL}
+                    method="POST"
+                    className="book-create-form"
+                >
+                    <input
+                        className={`input input--${theme}`}
+                        placeholder="Title"
+                        name="title"
+                        value={formData.title}
+                        onChange={(e) => HandleInputChange(e)}
+                    />
+
+                    <input
+                        className={`input input--${theme}`}
+                        placeholder="Author"
+                        name="author"
+                        value={formData.author}
+                        onChange={(e) => HandleInputChange(e)}
+                    />
+
+                    <input
+                        className={`input input--${theme} input__imageUrl--${theme}`}
+                        placeholder="Book Image link"
+                        name="imageUrl"
+                        value={formData.imageUrl}
+                        onChange={(e) => HandleInputChange(e)}
+                    />
+
+                    <section className="book-create-form__output-card">
+                        {errorBook && (
+                            <div className="validation__error-message">{errorBookDetail}</div>
+                        )}
+                        {bookCreatedMessage && (
+                            <div className="book-create-form__output-card">
+                                <div className="book-create-form__output">Book created!</div>
+                                <Link to="/">
+                                    <h3 className={`book-create-form__output--back-to-home--${theme}`}>
+                                        back to Home
+                                    </h3>
+                                </Link>
+                            </div>
+                        )}
+                    </section>
+                    <footer className="footer--flex">
+                        <button
+                            className="button book-create-form__cancel-button"
+                            onClick={() => {
+                                setErrorBook(false);
+                                setFormData({
+                                    title: "",
+                                    author: "",
+                                    imageUrl: "",
+                                });
+                            }}
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            className="button book-create-form__post-button"
+                            onClick={async () => {
+                                await PostBook();
+                            }}
+                        >
+                            Create book
+                        </button>
+                    </footer>
+                </form>
+            </section>
+        </>
+    );
 };
