68ab07549f9aa13022e00e05577f470d6359ce25
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-11-12 14:30:21 +0100

    refactor: Correct code indentation for readability and remove unnecessary code

--- frontend/LibraryFrontEnd/src/Component/NavBar/NavBar.tsx
+++ frontend/LibraryFrontEnd/src/Component/NavBar/NavBar.tsx
@@ -15,94 +15,88 @@ import { Settings } from "../Settings/Settings"
 
 type NavBarType = {
     theme: string,
-    handleTheme : () => void
+    handleTheme: () => void
 };
 
-export const NavBar = ({theme, handleTheme}: NavBarType) => {
+export const NavBar = ({ theme, handleTheme }: NavBarType) => {
     const [activeNavItem, setActiveNavItem] = useState<string>("home");
-    const [settingIsChecked, setSettingIsChecked] = useState<boolean>(false);
-    
 
-    const handleSettingCheck = () => {
-        setSettingIsChecked(!settingIsChecked);
-    }
-
-  return (
-    <>
-      <nav className={`navbar--grid navbar--${theme}`}>
-        <Link
-          className="nav-link nav__home__Link--flex"
-          to="/"
-          onClick={() => {
-            setActiveNavItem("home");
-          }}
-        >
-          <img
-            className="nav-link icone nav__HomeIcone"
-            src={activeNavItem === "home" ? homeBlueIcone : homeIcone}
-            alt="Home"
-          ></img>
-          <figcaption
-            className={
-              activeNavItem === "home"
-                ? "iconeTitle--blue nav__Home__title"
-                : "iconeTitle nav__Home__title"
-            }
-          >
-            Home
-          </figcaption>
-        </Link>
-        <Link
-          className="nav-link nav__search__link--flex"
-          to="/Search"
-          onClick={() => {
-            setActiveNavItem("search");
-          }}
-        >
-          <img
-            className="nav-link icone nav__search__image"
-            src={activeNavItem === "search" ? searchBlueIcone : searchIcone}
-            alt="Search"
-          />
-          <figcaption
-            className={
-              activeNavItem === "search"
-                ? "iconeTitle--blue nav__search__title"
-                : "iconeTitle nav__search__title"
-            }
-          >
-            Search
-          </figcaption>
-        </Link>
-        <Link
-          className="nav-link nav__addBook--flex"
-          to="/addBook"
-          onClick={() => {
-            setActiveNavItem("addBook");
-          }}
-        >
-          <img
-            className="nav-link icone nav__addBook"
-            src={activeNavItem === "addBook" ? bookBlueIcone : bookIcone}
-            alt="book"
-          />
-          <img
-            className="nav-link addIcone"
-            src={activeNavItem === "addBook" ? addBlueIcone : addIcone}
-            alt="add"
-          />
-          <figcaption
-            className={
-              activeNavItem === "addBook"
-                ? "iconeTitle--blue nav__AddBook__Title"
-                : "iconeTitle nav__AddBook__Title"
-            }
-          >
-            Add book
-          </figcaption>
-        </Link>       
-      </nav>
-      {activeNavItem === "settings" && (<Settings theme={theme} handleTheme={handleTheme} />)}
-    </>
-  );
+    return (
+        <>
+            <nav className={`navbar--grid navbar--${theme}`}>
+                <Link
+                    className="nav-link nav__home__Link--flex"
+                    to="/"
+                    onClick={() => {
+                        setActiveNavItem("home");
+                    }}
+                >
+                    <img
+                        className="nav-link icone nav__HomeIcone"
+                        src={activeNavItem === "home" ? homeBlueIcone : homeIcone}
+                        alt="Home"
+                    ></img>
+                    <figcaption
+                        className={
+                            activeNavItem === "home"
+                                ? "iconeTitle--blue nav__Home__title"
+                                : "iconeTitle nav__Home__title"
+                        }
+                    >
+                        Home
+                    </figcaption>
+                </Link>
+                <Link
+                    className="nav-link nav__search__link--flex"
+                    to="/Search"
+                    onClick={() => {
+                        setActiveNavItem("search");
+                    }}
+                >
+                    <img
+                        className="nav-link icone nav__search__image"
+                        src={activeNavItem === "search" ? searchBlueIcone : searchIcone}
+                        alt="Search"
+                    />
+                    <figcaption
+                        className={
+                            activeNavItem === "search"
+                                ? "iconeTitle--blue nav__search__title"
+                                : "iconeTitle nav__search__title"
+                        }
+                    >
+                        Search
+                    </figcaption>
+                </Link>
+                <Link
+                    className="nav-link nav__addBook--flex"
+                    to="/addBook"
+                    onClick={() => {
+                        setActiveNavItem("addBook");
+                    }}
+                >
+                    <img
+                        className="nav-link icone nav__addBook"
+                        src={activeNavItem === "addBook" ? bookBlueIcone : bookIcone}
+                        alt="book"
+                    />
+                    <img
+                        className="nav-link addIcone"
+                        src={activeNavItem === "addBook" ? addBlueIcone : addIcone}
+                        alt="add"
+                    />
+                    <figcaption
+                        className={
+                            activeNavItem === "addBook"
+                                ? "iconeTitle--blue nav__AddBook__Title"
+                                : "iconeTitle nav__AddBook__Title"
+                        }
+                    >
+                        Add book
+                    </figcaption>
+                </Link>
+            </nav>
+            {activeNavItem === "settings" && (<Settings theme={theme} handleTheme={handleTheme} />)}
+        </>
+    );
 };
