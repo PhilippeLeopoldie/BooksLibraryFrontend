c990cf927d356416ce6eb90cf0cd36200767fa40
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-11-12 14:16:48 +0100

    refactor: Correct code indentation for readability

--- frontend/LibraryFrontEnd/src/Component/BookCard/BookCover/BookCover.tsx
+++ frontend/LibraryFrontEnd/src/Component/BookCard/BookCover/BookCover.tsx
@@ -2,30 +2,30 @@ import "./BookCover.css";
 import { useState } from "react";
 
 type BookType = {
-  book: {
-    id: number;
-    title: string;
-    author: string;
-    averageRate: number;
-    imageUrl?: string;
-  };
+    book: {
+        id: number;
+        title: string;
+        author: string;
+        averageRate: number;
+        imageUrl?: string;
+    };
 };
 
 export const BookCover = ({ book }: BookType) => {
-  
-  const [imageZoom, setImageZoom] = useState<string>("zoomOut");
-  const toggleZoomImage = () => {
-    imageZoom === "zoomOut" ? setImageZoom("zoomIn") : setImageZoom("zoomOut");
-  };
 
-  return (
-    <>
-				<img
-					src={book.imageUrl && `${book.imageUrl}`}
-					className={`boocard__Image-${imageZoom} boocard__Image`}
-					alt="bookImage"
-					onClick={toggleZoomImage}
-				/>
-    </>
-  );
+    const [imageZoom, setImageZoom] = useState<string>("zoomOut");
+    const toggleZoomImage = () => {
+        imageZoom === "zoomOut" ? setImageZoom("zoomIn") : setImageZoom("zoomOut");
+    };
+
+    return (
+        <>
+            <img
+                src={book.imageUrl && `${book.imageUrl}`}
+                className={`boocard__Image-${imageZoom} boocard__Image`}
+                alt="bookImage"
+                onClick={toggleZoomImage}
+            />
+        </>
+    );
 };
