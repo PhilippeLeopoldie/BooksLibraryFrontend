e81010e930d2f41750da9c0685a1d47ed31e6735
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-11-12 14:15:21 +0100

    refactor: Define section for improved organization and correct code indentation for readability

--- frontend/LibraryFrontEnd/src/Component/BookCard/BookBody/BookBody.tsx
+++ frontend/LibraryFrontEnd/src/Component/BookCard/BookBody/BookBody.tsx
@@ -1,20 +1,19 @@
 import "./BookBody.css";
 type BookTitleAuthor = {
-  title: string;
-  author: string;
+    title: string;
+    author: string;
 }
 
 
-export const BookBody = ({title,author} : BookTitleAuthor) => {
-  return (
-    <div className="bookcard__body">
-      <h3 title="Book Title" className="booktitle">
-          {title.length > 40
-            ? `${title.slice(0, 40)}...`
-            : title}
-        </h3>
-        <h3 className="bookauthor">by: {author}</h3>
-      
-    </div>
-  )
+export const BookBody = ({ title, author }: BookTitleAuthor) => {
+    return (
+        <section className="bookcard__body">
+            <h3 title="Book Title" className="booktitle">
+                {title.length > 40
+                    ? `${title.slice(0, 40)}...`
+                    : title}
+            </h3>
+            <h3 className="bookauthor">by: {author}</h3>
+        </section>
+    )
 };
