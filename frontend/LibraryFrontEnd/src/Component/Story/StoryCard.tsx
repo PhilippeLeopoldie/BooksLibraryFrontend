e194850990bb991ebdd8e221628f3e5ad4c9b3aa
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-12-03 01:27:25 +0100

    Refactor: Remove unnecessary code and adjust styles

--- frontend/LibraryFrontEnd/src/Component/Story/StoryCard.tsx
+++ frontend/LibraryFrontEnd/src/Component/Story/StoryCard.tsx
@@ -1,21 +1,16 @@
+import { Context, useContext } from "react";
 import { ThemeContext } from "../App/App";
+import { AiUserPreferences } from "../Preferences/AiUserPreferences/AiUserPreferences";
+import { AiStory } from "../Story/AiStory/AiStory";
 import "./StoryCard.css"
 
 export const StoryCard = () => {
-    
+    const theme = useContext(ThemeContext);
     return (
-        <>
-            <ThemeContext.Consumer >
-                {(theme) => (
-                    <section className={`StoryCard_container StoryCard_container--${theme}`}>
-                        <header>Create your own short story</header>
-                        <body>
-                            
-                        </body>
-                    </section>
-                )
-            }
-            </ThemeContext.Consumer>            
-        </>
+        <section className={`StoryCard_container StoryCard_container--${theme}`}>
+                <h2 className={`StoryCard_header--${theme}`}>Create your own short story</h2>
+                <AiUserPreferences />
+                <AiStory/>  
+        </section>
     );
 }
