7b608d4ceeaed45f0c121910b0ab83636b4ffc72
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2024-12-30 11:13:38 +0100

    Update: Add AiStory development message and apply style

--- frontend/LibraryFrontEnd/src/Component/Story/AiStory/AiStory.tsx
+++ frontend/LibraryFrontEnd/src/Component/Story/AiStory/AiStory.tsx
@@ -1,7 +1,25 @@
+import "./AiStory.css";
+import { ThemeContext } from "../../../App/App";
+import { useContext, useState } from "react";
+
 export const AiStory = () => {
+    const theme = useContext(ThemeContext);
+    const [activated, setActivated] = useState<Boolean>(false);
+
+    const activateStory = () => {
+        setActivated(!activated);
+    }
     return (
         <>
-            <button className="generateStory">Generate Story</button>
+            <button
+                className="generateStory"
+                onClick={activateStory}
+            >Generate Story</button>
+            <section className={`AiStory--${theme} AiStory`}>
+                <h3 className={`AiStory--${theme} AiStory__activated--${activated}`}>
+                    Oups! This feature is still in development. Come back soon!
+                </h3>
+            </section>
         </>
     )
 }
