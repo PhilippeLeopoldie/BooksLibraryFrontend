8bca1b5872c1eadea4ce81dc050be6d84c198e2e
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-02-02 22:42:09 +0100

    refactor: Create a dedicated component for TopBooks

--- frontend/LibraryFrontEnd/src/Component/HomePage/HomePage.tsx
+++ frontend/LibraryFrontEnd/src/Component/HomePage/HomePage.tsx
@@ -1,19 +1,17 @@
 import { BookCard } from "../BookCard/BookCard";
 import "./HomePage.css";
 import { BOOK_URL } from "../../constants/api";
-import { BOOK_TOP_BOOK_URL } from "../../constants/api";
 import { BookType, PaginatedBookType, PaginationType } from "../../constants/types";
 import { getPaginatedItemsUrl } from "../../constants/commonFunctions";
+import { TopBook } from "./TopBooks/TopBook";
 import { useContext, useEffect, useState } from "react";
-import { newBooksCacheContext, ThemeContext, topBooksCacheContext } from "../../App/App";
+import { newBooksCacheContext, ThemeContext } from "../../App/App";
 
 export const HomePage = () => {
     const theme = useContext(ThemeContext);
     const [pagination, setPagination] = useState<PaginationType>({ page:"1", pageSize:"6" })
     const newBooksCache = useContext(newBooksCacheContext);
-    const topBooksCache = useContext(topBooksCacheContext);
     const [newBooks, setNewBooks] = useState<BookType[] | undefined | null>(newBooksCache?.newBooksCache);
-    const [topBooks, setTopBooks] = useState<BookType[] | undefined | null>(topBooksCache?.topBooksCache);
     const [numberOfBooks, setNumberOfBooks] = useState<number>(3);
     
     const fetchNewBooks = async () => {
@@ -32,39 +30,19 @@ export const HomePage = () => {
         }
     };
 
-    const fetchTopBook = async () => {
-        try {
-            const topBookResponse: Response = await fetch(`${BOOK_TOP_BOOK_URL}${numberOfBooks}`);
-            if (topBookResponse.status === 200) {
-                const topBooksResponseData = await topBookResponse.json();
-                setTopBooks(topBooksResponseData.books);
-                topBooksCache?.setTopBooksCache(topBooksResponseData.books);
-            } else if (topBookResponse.status === 404) {
-                console.log(topBookResponse);
-            }
-        } catch (error) {
-            console.error("Error fetching TopBook:", error);
-        }
-    };
-
     useEffect(() => {
         if (!newBooks || newBooks.length === 0) fetchNewBooks();
-        if (!topBooks) fetchTopBook();
+        
     }, []);
 
-    if (!newBooks || newBooks.length === 0 || !topBooks) {
+    if (!newBooks || newBooks.length === 0 ) {
         return <h1 className={"Books__Loading--" + theme}>Loading...</h1>;
     }
 
     return (
         <>
             <div className={`BookList--${theme}--flex`}>
-                <h1 className="BookListTitle">Popular</h1>
-                <div className="bookListContainer">
-                    {topBooks?.map((topBook) => (
-                        <BookCard key={topBook.id} book={topBook} />
-                    ))}
-                </div>
+                <TopBook maxNumBooks={`${numberOfBooks}`} />
                 <h1 className="BookListTitle">Recent</h1>
                 <div className="bookListContainer">
                     {newBooks &&
---
+++ frontend/LibraryFrontEnd/src/Component/HomePage/TopBooks/TopBook.tsx
@@ -0,0 +1,46 @@
+import { BookCard } from "../../BookCard/BookCard";
+import { BookType, PaginatedBookType, PaginationType } from "../../../constants/types";
+import { useContext, useEffect, useState } from "react";
+import { ThemeContext, topBooksCacheContext } from "../../../App/App";
+import { BOOK_TOP_BOOK_URL } from "../../../constants/api";
+
+
+export const TopBook = ({ maxNumBooks }: { maxNumBooks: string })  => {
+    const theme = useContext(ThemeContext);
+    const topBooksCache = useContext(topBooksCacheContext);
+    const [topBooks, setTopBooks] = useState<BookType[] | undefined | null>(topBooksCache?.topBooksCache);
+
+    const fetchTopBook = async () => {
+        try {
+            const topBookResponse: Response = await fetch(`${BOOK_TOP_BOOK_URL}${maxNumBooks}`);
+            if (topBookResponse.status === 200) {
+                const topBooksResponseData = await topBookResponse.json();
+                setTopBooks(topBooksResponseData.books);
+                topBooksCache?.setTopBooksCache(topBooksResponseData.books);
+            } else if (topBookResponse.status === 404) {
+                console.log(topBookResponse);
+            }
+        } catch (error) {
+            console.error("Error fetching TopBook:", error);
+        }
+    };
+
+    useEffect(() => {
+        if (!topBooks) fetchTopBook();
+    }, []);
+
+    if (!topBooks) {
+        return <h1 className={"Books__Loading--" + theme}>Loading...</h1>;
+    }
+
+    return (
+        <>
+            <h1 className="BookListTitle">Popular</h1>
+            <div className="bookListContainer">
+                {topBooks?.map((topBook) => (
+                    <BookCard key={topBook.id} book={topBook} />
+                ))}
+            </div>
+        </>
+    )
+};
