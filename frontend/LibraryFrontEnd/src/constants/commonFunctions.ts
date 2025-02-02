7d2a15acb5e715b9785f316eda2bc227d5f60a56
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-02-02 19:35:57 +0100

    feat: Implement getPaginatedItems fonction for reusability

--- frontend/LibraryFrontEnd/src/constants/commonFunctions.ts
+++ frontend/LibraryFrontEnd/src/constants/commonFunctions.ts
@@ -5,3 +5,7 @@ export const numberOfReviewsValidation = (numberOfReview: number | undefined) =>
     if (numberOfReview && numberOfReview === 1) return `${numberOfReview} review`;
     if (numberOfReview && numberOfReview > 1) return `${numberOfReview} reviews`;
 }
+
+export const getPaginatedItemsUrl = (url: string, page: string, pageSize: string) => {
+    return `${url}?Page=${page}&PageSize=${pageSize}`;
+}
