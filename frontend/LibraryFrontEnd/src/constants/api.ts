51f3972ceb427da0e7f64d13594cbe098b6fb863
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-02-03 18:21:58 +0100

    style: Adjust padding and gap to genreFilter

--- frontend/LibraryFrontEnd/src/constants/api.ts
+++ frontend/LibraryFrontEnd/src/constants/api.ts
@@ -4,6 +4,7 @@ export const BOOK_URL: string = API_URL + "Books";
 export const BOOK_TOP_BOOK_URL : string = BOOK_URL+"/TopBooks?numberOfBooks="
 export const BOOK_BY_BOOKID_URL: string = BOOK_URL+"/";
 export const BOOK_BY_TITLE_OR_AUTHOR_URL : string = BOOK_URL+"/TitleOrAuthor?titleOrAuthor=";
+export const BOOKS_BY_GENRESId_URL: string = `${BOOK_URL}/genre?genresId=`
 export const OPINION_URL: string = API_URL+"Opinions/";
 export const OPINION_BY_BOOKID_URL : string = OPINION_URL+"BookId=";
 export const GENRES_LIST_URL: string = API_URL+"Genres";
