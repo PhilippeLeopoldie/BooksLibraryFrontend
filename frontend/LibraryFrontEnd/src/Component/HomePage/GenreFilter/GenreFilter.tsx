5b6c094dba05996972b5bf115e912bfe4af266b3
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-02-03 13:12:16 +0100

    feat: Create GenreFilter component

---
+++ frontend/LibraryFrontEnd/src/Component/HomePage/GenreFilter/GenreFilter.tsx
@@ -0,0 +1,40 @@
+import { GenreButton } from "../../GenreButton/GenreButton";
+import { useContext, useEffect, useState } from "react";
+import { ThemeContext, genresCacheContext } from "../../../App/App";
+
+export const GenreFilter = () => {
+    const theme = useContext(ThemeContext);
+    const listOfGenresContext = useContext(genresCacheContext);
+    const listOfGenres = listOfGenresContext?.genresCache?.genres || [];
+    const [genreFilter, setGenreFilter] = useState<string>(
+        sessionStorage.getItem("genreFiltered") || ""
+    );
+
+    const handleGenreFiltered = (genreId: string) => {
+        sessionStorage.setItem("genreFiltered", genreId);
+        setGenreFilter(genreId);
+    }
+
+    useEffect(() => {
+        console.log(genreFilter);
+    }, [genreFilter]);
+
+
+    return (
+        <>
+            <section className={`genreFilter_container--${theme}`}>
+                {
+                    listOfGenres.map((genre) =>
+                        <GenreButton
+                            key={genre.id}
+                            genre={genre}
+                            handleGenres={handleGenreFiltered}
+                            typeOfChoice='single'
+                            sessionStorageName='genreFiltered'
+                        />)
+                }
+            </section>
+
+        </>
+    )
+}
