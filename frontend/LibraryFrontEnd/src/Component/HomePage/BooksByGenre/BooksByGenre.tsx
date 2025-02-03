97eb977bfa38ea9b3fd9afa4b87358a250aa6dc9
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-02-03 22:11:29 +0100

    feat: Encapsulate content in a state variable for better flexibility

--- frontend/LibraryFrontEnd/src/Component/HomePage/BooksByGenre/BooksByGenre.tsx
+++ frontend/LibraryFrontEnd/src/Component/HomePage/BooksByGenre/BooksByGenre.tsx
@@ -1,15 +1,17 @@
 import { BookCard } from "../../BookCard/BookCard";
 import { BOOKS_BY_GENRESId_URL } from "../../../constants/api"
-import { useEffect, useState } from "react";
+import { useContext ,useEffect, useState } from "react";
 import { BookType, PaginatedBookType ,PaginationType } from "../../../constants/types";
 import { getPaginatedItemsUrl } from "../../../constants/commonFunctions";
+import { ThemeContext } from "../../../App/App";
 
 
 export const BooksByGenre = ({ sessionStorageName }: { sessionStorageName: string }) => {
+    const theme = useContext(ThemeContext);
     const selectedGenre = sessionStorage.getItem(sessionStorageName);
     const [pagination, setPagination] = useState<PaginationType>({ page: "1", pageSize: "6" })
     const [booksByGenre, setBooksByGenre] = useState<BookType[] | undefined >();
-
+    const [displayedContent, setDisplayedContent] = useState<JSX.Element>(<></>);
     const fetchBooksByGenreId = async () => {
         try {
             const booksByIdResponse: Response =
@@ -17,10 +19,21 @@ export const BooksByGenre = ({ sessionStorageName }: { sessionStorageName: strin
             if (booksByIdResponse.status === 200) {
                 const booksData: PaginatedBookType = await booksByIdResponse.json();
                 setBooksByGenre(booksData.paginatedItems);
+                if (booksData.paginatedItems &&
+                    Array.isArray(booksData.paginatedItems)) {
+                    setDisplayedContent(
+                        <>
+                            {booksData.paginatedItems.map((book) => (
+                                <BookCard key={book.id} book={book} />
+                            ))}
+                        </>
+                    )
+                };
             } else if (booksByIdResponse.status === 400) {
                 console.log(booksByIdResponse);
+                setDisplayedContent(<h3> No Books found for this genre!</h3>);
             }
-            
+
         } catch (error) {
             console.error("Error fetching books", error);
         }
@@ -30,16 +43,9 @@ export const BooksByGenre = ({ sessionStorageName }: { sessionStorageName: strin
         fetchBooksByGenreId();
     }, [selectedGenre])
 
-
     return (
-        <>
-            {
-                booksByGenre &&
-                Array.isArray(booksByGenre) &&
-                booksByGenre.map((book) => (
-                    <BookCard key={book.id} book={book} />
-                ))
-            }
-        </>
+        <div className={`booksByGenre_container--${theme}` }>
+            {displayedContent}
+        </div>
     )
 }
