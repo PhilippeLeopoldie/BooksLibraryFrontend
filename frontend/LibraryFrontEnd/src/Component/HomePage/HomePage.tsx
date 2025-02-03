1ba4b0f4206148438674857f13fcf36e1bd31b9d
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-02-03 13:32:58 +0100

    feat: Add genreFilter to home page

--- frontend/LibraryFrontEnd/src/Component/HomePage/HomePage.tsx
+++ frontend/LibraryFrontEnd/src/Component/HomePage/HomePage.tsx
@@ -1,4 +1,5 @@
 import "./HomePage.css";
+import { GenreFilter } from "./GenreFilter/GenreFilter";
 import { NewBooks } from "./NewBooks/NewBooks";
 import { TopBook } from "./TopBooks/TopBook";
 import { useContext, useState } from "react";
@@ -13,6 +14,7 @@ export const HomePage = () => {
     return (
         <>
             <div className={`BookList--${theme}--flex`}>
+                <GenreFilter/>
                 <TopBook maxNumBooks={`${numberOfBooks}`} />
                 <NewBooks/>
             </div>
