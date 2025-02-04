f910af08a91f3369a38fe18fde858c99b0e6f5e7
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-02-04 14:50:30 +0100

    feat: Implement conditionnal rendering based on selected genre

--- frontend/LibraryFrontEnd/src/Component/HomePage/HomePage.tsx
+++ frontend/LibraryFrontEnd/src/Component/HomePage/HomePage.tsx
@@ -1,22 +1,40 @@
 import "./HomePage.css";
+import { BooksByGenre } from "./BooksByGenre/BooksByGenre";
 import { GenreFilter } from "./GenreFilter/GenreFilter";
 import { NewBooks } from "./NewBooks/NewBooks";
 import { TopBook } from "./TopBooks/TopBook";
-import { useContext, useState } from "react";
-import { ThemeContext } from "../../App/App";
+import { useContext, useEffect, useState } from "react";
+import { FilteredGenreContext, ThemeContext} from "../../App/App";
 
 export const HomePage = () => {
     const theme = useContext(ThemeContext);
+    const genreContext = useContext(FilteredGenreContext);
     const [numberOfBooks, setNumberOfBooks] = useState<number>(3);
+    const [displayedContent, setDisplayedContent] = useState<JSX.Element>(<></>);
+
     
-    
+    useEffect(() => {
+        if (genreContext && genreContext.genreFilter !== 'All') {
+            setDisplayedContent(
+                <section className={`booksByGenre_container booksByGenre_container--${theme}`}>
+                    <BooksByGenre sessionStorageName={"genreFiltered"} />
+                </section>
+            );
+        } else {
+            setDisplayedContent(
+                <>
+                    <TopBook maxNumBooks={`${numberOfBooks}`} />
+                    <NewBooks />
+                </>
+            );
+        }
+    }, [genreContext?.genreFilter])
 
     return (
         <>
             <div className={`BookList--${theme}--flex`}>
-                <GenreFilter/>
-                <TopBook maxNumBooks={`${numberOfBooks}`} />
-                <NewBooks/>
+                <GenreFilter />
+                {displayedContent}
             </div>
         </>
     );
