bf252d1e0252ff719b772e0353131ebb0acbfe4b
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-02-08 18:49:28 +0100

    refactor: Eliminate redundancy and enhance naming consistency

--- frontend/LibraryFrontEnd/src/Component/HomePage/NewBooks/NewBooks.tsx
+++ frontend/LibraryFrontEnd/src/Component/HomePage/NewBooks/NewBooks.tsx
@@ -3,27 +3,25 @@ import { BookType, PaginatedBookType, PaginationType } from "../../../constants/
 import { BOOK_URL } from "../../../constants/api";
 import { getPaginatedItemsUrl } from "../../../constants/commonFunctions";
 import "./NewBooks.css";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useRef } from "react";
 import { ThemeContext, paginatedBooksCacheContext } from "../../../App/App";
 
 
 export const NewBooks = ()  => {
     const theme = useContext(ThemeContext);
-    const paginatedBooksCache = useContext(paginatedBooksCacheContext);
+    const paginatedBooksContext = useContext(paginatedBooksCacheContext);
     const [pagination, setPagination] = useState<PaginationType>({ page: "1", pageSize: "6" });
-    const [paginatedBooks, setPaginatedBooks] = useState<PaginatedBookType | undefined | null>(paginatedBooksCache?.paginatedBooks);
-    const [initialBooks, setInitialBooks] = useState<BookType[] | undefined | null>(paginatedBooksCache?.paginatedBooks?.paginatedItems);
+    const [paginatedBooks, setPaginatedBooks] = useState<PaginatedBookType | undefined | null>(paginatedBooksContext?.paginatedBooks);
 
     const fetchNewBooks = async (page: string, pageSize: string) => {
         try {
-            const newBooksResponse: Response = await fetch(
-                getPaginatedItemsUrl(`${BOOK_URL}?`, page, pageSize));
+            const newBooksResponse: Response = await fetch(getPaginatedItemsUrl(`${BOOK_URL}?`, page, pageSize));
+
             if (newBooksResponse.status === 200) {
                 const newBooksResponseData: PaginatedBookType = await newBooksResponse.json();
-                const concatenedItems = initialBooks ?
-                    initialBooks.concat(newBooksResponseData.paginatedItems)
-                    :
-                    newBooksResponseData.paginatedItems;
+                const concatenedItems = paginatedBooks?.paginatedItems
+                        ? paginatedBooks.paginatedItems.concat(newBooksResponseData.paginatedItems)
+                        : newBooksResponseData.paginatedItems;
                 saveBooks({...newBooksResponseData,paginatedItems: concatenedItems});
             } else if (newBooksResponse.status === 404) {
                 console.log(newBooksResponse);
@@ -33,27 +31,26 @@ export const NewBooks = ()  => {
         }
     };
 
-    const saveBooks = (updatedBooks: PaginatedBookType) => {
-        setPaginatedBooks(updatedBooks);
-        setInitialBooks(updatedBooks.paginatedItems);
-        paginatedBooksCache?.setPaginatedBooks(updatedBooks);
+    const saveBooks = (updatedPaginatedBooks: PaginatedBookType) => {
+        setPaginatedBooks(updatedPaginatedBooks);
+        paginatedBooksContext?.setPaginatedBooks(updatedPaginatedBooks);
     }
 
     const goToNextPage = () => {
         const nextPageNumber = (paginatedBooks ? paginatedBooks?.page + 1 : 1);
-        if (paginatedBooks && nextPageNumber <= paginatedBooks?.totalPages) {
+        if (paginatedBooks && nextPageNumber <= paginatedBooks.totalPages) {
             setPagination((prev) => ({ ...prev, page: nextPageNumber.toString() }))
             fetchNewBooks(nextPageNumber.toString(), pagination.pageSize);
         }
     }
     
     useEffect(() => {
-        if (!initialBooks || initialBooks.length === 0) {
+        if (!paginatedBooks || paginatedBooks?.paginatedItems.length === 0) {
             fetchNewBooks(pagination.page, pagination.pageSize)
         }
     }, []);
 
-    if (!initialBooks) {
+    if (!paginatedBooks?.paginatedItems) {
         return <h1 className={"Books__Loading--" + theme}></h1>;
     }
 
@@ -61,9 +58,8 @@ export const NewBooks = ()  => {
         <>
             <h1 className="BookListTitle">Recent</h1>
                 <div className="bookListContainer--flex">
-                    {initialBooks &&
-                        Array.isArray(initialBooks) &&
-                        initialBooks
+                {paginatedBooks.paginatedItems &&
+                    paginatedBooks.paginatedItems
                             .sort((previousNewBook, lastNewBook) => lastNewBook.id - previousNewBook.id)
                             .map((newBook) => (
                             <BookCard key={newBook.id} book={newBook} />
