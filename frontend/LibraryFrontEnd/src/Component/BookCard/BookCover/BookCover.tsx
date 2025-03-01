75c979bdb62a307a9732bb23b6f569d8ea499f10
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-03-01 19:08:21 +0100

    feat(bookCard) : Display book description when clicking the cover

--- frontend/LibraryFrontEnd/src/Component/BookCard/BookCover/BookCover.tsx
+++ frontend/LibraryFrontEnd/src/Component/BookCard/BookCover/BookCover.tsx
@@ -1,22 +1,35 @@
 import "./BookCover.css";
 import { BookType } from "../../../constants/types";
-import { useState } from "react";
+import { ThemeContext } from "../../../App/App";
+import { useContext,useState } from "react";
+
+type CurentView = 'bookCover' | 'description';
 
 export const BookCover = ({ book }: { book:BookType }) => {
-
-    const [imageZoom, setImageZoom] = useState<string>("zoomOut");
-    const toggleZoomImage = () => {
-        imageZoom === "zoomOut" ? setImageZoom("zoomIn") : setImageZoom("zoomOut");
-    };
+    const theme = useContext(ThemeContext);
+    const [currentView, setCurrentView] = useState<CurentView>('bookCover');
+    const toggleDescription = () => {
+        setCurrentView(currentView === 'bookCover' ? 'description' : 'bookCover');
+    }
 
     return (
         <>
-            <img
-                src={book.imageUrl && `${book.imageUrl}`}
-                className={`boocard__Image-${imageZoom} boocard__Image`}
-                alt="bookImage"
-                onClick={toggleZoomImage}
-            />
+            {currentView === 'bookCover' ? (
+                <img
+                    src={book.imageUrl && `${book.imageUrl}`}
+                    className={`bookcard__Image`}
+                    alt="bookImage"
+                    onClick={toggleDescription}
+                />
+            ) : (
+                <div
+                    className="bookcard__Description"
+                    onClick={toggleDescription}
+                    >
+                        <h2 className={`bookcard__DescriptionTitle--${theme}` }>Description</h2>
+                        <p className={`bookcard__Description bookcard__Description--${theme}`}>{book.description || "No description available"}</p>
+                    </div>
+                )}
         </>
     );
 };
