ff64fbdec50af50de4f2ee214b7e64e1c828cc18
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-03-02 17:49:34 +0100

    feat: Add speedInsights component to collect performance metrics

--- frontend/LibraryFrontEnd/src/index.tsx
+++ frontend/LibraryFrontEnd/src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App/App'
 import './index.css' 
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights/>
   </React.StrictMode>,
 )
