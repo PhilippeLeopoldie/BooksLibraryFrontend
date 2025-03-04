8dbd134c888123a12030e9b25c2e3532c0c108c4
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-03-05 00:38:39 +0100

    feat(Storycard): Move AiStory component to AiUserPreferences

--- frontend/LibraryFrontEnd/src/Component/Story/StoryCard.tsx
+++ frontend/LibraryFrontEnd/src/Component/Story/StoryCard.tsx
@@ -1,7 +1,6 @@
 import { Context, useContext } from "react";
 import { ThemeContext } from "../../App/App";
 import { AiUserPreferences } from "../Preferences/AiUserPreferences/AiUserPreferences";
-import { AiStory } from "../Story/AiStory/AiStory";
 import "./StoryCard.css"
 
 export const StoryCard = () => {
@@ -10,7 +9,6 @@ export const StoryCard = () => {
         <section className={`StoryCard_container StoryCard_container--${theme}`}>
                 <h2 className={`StoryCard_header--${theme}`}>Generate your own A.I. Story</h2>
                 <AiUserPreferences />
-                <AiStory/>  
         </section>
     );
 }
