7f2c23a90dc58cb6181ae9158a76c64ed4a73e1b
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-03-09 12:37:49 +0100

    feat(About): Add OpenAI icone with link to amphasize AI integration

--- frontend/LibraryFrontEnd/src/Component/About/About.tsx
+++ frontend/LibraryFrontEnd/src/Component/About/About.tsx
@@ -1,20 +1,27 @@
 import { useContext } from "react";
 import { ThemeContext } from "../../App/App";
 import "./About.css";
-import openAiSvg from "../../media/openai.svg";
+import openAiSvg from "../../media/openAi.svg";
+import { Link } from "react-router-dom";
 
 export const About = () => {
     const theme = useContext(ThemeContext);
     return (
         <>
-            <section className={`About About--${theme} About_container`}>
-                <h2 className={`About About--${theme} About_title`}>ReadSphere</h2>
-                <article className={`About About--${theme} About_article`}>
-                    Your Personalized Short Stories and Book Library Platform<br /> 
+            <section className={`instructions instructions--${theme} About_container`}>
+                <h2 className={`instructions instructions--${theme} About_title`}>ReadSphere<br/><br/></h2>
+                <article className={`instructions instructions--${theme} About_article`}>
+                    Your Personalized Short Stories and Book Library Platform.<br/> 
                     <br/>
                     Featuring a sleek interface, curated book recommendations,
-                    and an exciting integration with OpenAI <img src={openAiSvg} alt="openAiImage"/> to generate your own short stories,
-                    <br/>
+                    and an exciting integration with {' '}
+                    <Link className={`instructions instructions--${theme} OpenAiLink`} to="https://openai.com/">
+                        <span className={`instructions OpenAiText`} >OpenAI
+                            <img className="icone openAiIcine" src={openAiSvg} alt="openAiImage" />
+                        </span> 
+                    </Link>
+                    {' '}to generate your own short stories.<br />
+                    <br />
                     ReadSphere makes it effortless to discover books and stories tailored to your unique interests.<br/>
                     <br/>
                     Whether you're searching by title, author, or genre, or exploring top-rated reads,
