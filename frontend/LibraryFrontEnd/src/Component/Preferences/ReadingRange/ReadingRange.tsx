1e92dd384ff02f1c6d3d764d9d2ae6f9eca9fefc
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-03-09 12:52:34 +0100

    refactor(ReadingRange): Improve indentation and formatting

--- frontend/LibraryFrontEnd/src/Component/Preferences/ReadingRange/ReadingRange.tsx
+++ frontend/LibraryFrontEnd/src/Component/Preferences/ReadingRange/ReadingRange.tsx
@@ -1,5 +1,5 @@
 import "./ReadingRange.css"
-import  { ChangeEvent,useContext,useState } from "react";
+import { ChangeEvent, useContext, useState } from "react";
 import { ThemeContext } from "../../../App/App";
 
 type HandleReadingTimeType = {
@@ -14,26 +14,29 @@ export const ReadingRange = ({ readingTimeHandler }: HandleReadingTimeType) => {
     const handleRangeValue = (event: ChangeEvent<HTMLInputElement>) => {
         const rawvalue = Number(event.target.value);
 
-        const closestValue = allowedvalues.reduce((prev, curr) => 
+        const closestValue = allowedvalues.reduce((prev, curr) =>
             Math.abs(curr - rawvalue) < Math.abs(prev - rawvalue) ? curr : prev);
 
         setRangeValue(closestValue.toString());
         readingTimeHandler(closestValue.toString());
     }
-        return (
-            <>
-                <section className={`ReadingRange--flex ReadingRange--flex--${theme}`}>
-                    <p className={`instructions instructions--${theme}`}>Adjust the slider to choose your reading time:</p>
-                    <label className="RangeLabel" htmlFor="Reading_time_slider">{`${rangeValue} min`}
-                    </label>
-                    <input
-                        className="Reading_time_slider"
-                        type="range" min="1" max="21" step="5"
-                        value={rangeValue}
-                        onChange={handleRangeValue}
-                    >
-                    </input>
-                </section>
-            </>
-        );
-    }
+    return (
+        <>
+            <section className={`ReadingRange--flex ReadingRange--flex--${theme}`}>
+                <p className={`instructions instructions--${theme}`}>Adjust the slider to choose your reading time:</p>
+                <label className="RangeLabel" htmlFor="Reading_time_slider">{`${rangeValue} min`}
+                </label>
+                <input
+                    className="Reading_time_slider"
+                    type="range"
+                    min="1"
+                    max="21"
+                    step="5"
+                    value={rangeValue}
+                    onChange={handleRangeValue}
+                >
+                </input>
+            </section>
+        </>
+    );
+}
