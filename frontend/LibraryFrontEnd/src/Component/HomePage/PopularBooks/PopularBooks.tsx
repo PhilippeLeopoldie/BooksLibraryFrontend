1f37b7205e345f8f04923c444dc6f0b24a9b5e72
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-03-13 22:41:38 +0100

    feat(PopularBooks): Replace waiting message with `WaitingImage` component

--- frontend/LibraryFrontEnd/src/Component/HomePage/PopularBooks/PopularBooks.tsx
+++ frontend/LibraryFrontEnd/src/Component/HomePage/PopularBooks/PopularBooks.tsx
@@ -3,6 +3,8 @@ import { BookType, PaginatedBookType, PaginationType } from "../../../constants/
 import { useContext, useEffect, useState } from "react";
 import { ThemeContext, topBooksCacheContext } from "../../../App/App";
 import { BOOK_TOP_BOOK_URL } from "../../../constants/api";
+import OpenAi from "../../../media/openAi.svg";
+import { WaitingImage } from "../../../constants/WaitingImage/WaitingImage"; 
 
 
 export const PopularBooks = ({ maxNumBooks }: { maxNumBooks: string })  => {
@@ -30,7 +32,9 @@ export const PopularBooks = ({ maxNumBooks }: { maxNumBooks: string })  => {
     }, []);
 
     if (!topBooks) {
-        return <h1 className={"Books__Loading--" + theme}>Loading...</h1>;
+        return <div className={"Books__Loading--" + theme}>
+            <WaitingImage waitingImage={OpenAi} />
+        </div>;
     }
 
     return (
