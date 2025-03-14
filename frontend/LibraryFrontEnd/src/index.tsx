857262c7a71e6da894ea9ef8bc89e68dabf9763a
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-03-14 19:08:32 +0100

    feat(index): Add analytics to strack counting visitors and page views

--- frontend/LibraryFrontEnd/src/index.tsx
+++ frontend/LibraryFrontEnd/src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App/App'
 import './index.css' 
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-    <SpeedInsights/>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+        <SpeedInsights />
+        <Analytics />
+    </React.StrictMode>,
 )
