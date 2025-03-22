a4a9b5adb3a777766d7bc2e5bd76fab2ff7b56b3
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-03-22 20:23:50 +0100

    fix(types): Remove 'as const' from languages to avoid incompatibility with '.toSorted' function.

--- frontend/LibraryFrontEnd/src/constants/types.ts
+++ frontend/LibraryFrontEnd/src/constants/types.ts
@@ -40,5 +40,5 @@ export type PaginationType = {
 //Dynamically infer AistoryLanguageType from languages values
 export type AiStoryLanguageType = typeof languages[number];
 
-const languages = ["English", "French", "Swedish", "Spanish", "Italian", "Turkish"] as const;
+const languages = ["English", "French", "Swedish", "Spanish", "Italian", "Turkish"];
 export const languagesArray = languages.toSorted();
